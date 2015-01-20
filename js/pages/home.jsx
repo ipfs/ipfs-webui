79d00cb8c2788ee6ae016d5956af409f8dd1e2ea
Matt Bell <mappum@gmail.com>
2015-01-19 21:25:19 -0800

    Added gateway enable/disable checkbox

--- js/pages/home.jsx
+++ js/pages/home.jsx
@@ -8,20 +8,41 @@ var TabPane = require('react-bootstrap/TabPane')
 module.exports = React.createClass({
   getInitialState: function() {
     var t = this
-    var id = t.props.ipfs.id(function(err, id) {
-      console.log(id)
+    t.props.ipfs.id(function(err, id) {
       if(err || !id) return console.error(err)
       t.setState(id)
+    });
+
+    t.props.ipfs.config.get('Gateway.Enabled', function(err, enabled) {
+      if(err) return console.error(err);
+      t.setState({ GatewayEnabled: enabled.Value });
     });
 
     return {
       ID: '',
       Addresses: [],
-      AgentVersion: ''
+      AgentVersion: '',
+      GatewayEnabled: false,
+      GatewayUrl: 'http://localhost:8888/'
     }
   },
 
+  onGatewayChange: function(e) {
+    var t = this;
+    var enabled = !t.state.GatewayEnabled;
+    var api = enabled ? t.props.ipfs.gateway.enable : t.props.ipfs.gateway.disable;
+    api(function(err) {
+      if(err) return console.error(err);
+      t.setState({ GatewayEnabled: enabled });
+    });
+  },
+
   render: function() {
+    var gatewayEnabled = this.state.GatewayEnabled;
+    var gatewayLink;
+    if(gatewayEnabled)
+      gatewayLink = <a href={this.state.GatewayUrl}>Go to gateway</a>
+
     return (
   <div className="row">
     <div className="col-sm-10 col-sm-offset-1">
@@ -34,6 +55,17 @@ module.exports = React.createClass({
       <h4>Network Addresses</h4>
       <div className="panel panel-default">
         {Table({ table: this.state.Addresses })}
+      </div>
+
+      <div className="well">
+        <h4>HTTP Gateway</h4>
+        <div className="checkbox">
+          <label>
+            <input type="checkbox" class="gateway-toggle" checked={gatewayEnabled} onChange={this.onGatewayChange}/>
+            <strong>Enabled</strong>
+          </label>
+        </div>
+        {gatewayLink}
       </div>
 
     </div>
