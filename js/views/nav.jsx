cecc21188c994938ecdef57063df6d99054ed08f
Matt Bell <mappum@gmail.com>
2015-01-24 02:55:25 -0800

    Hide tabs for incomplete pages

--- js/views/nav.jsx
+++ js/views/nav.jsx
@@ -10,8 +10,8 @@ module.exports = React.createClass({
         <li><Link to="connections"><i className="fa fa-share-alt"></i> Connections</Link></li>
         <li><Link to="files"><i className="fa fa-copy"></i> Files</Link></li>
         <li><Link to="objects"><i className="fa fa-cubes"></i> Objects</Link></li>
-        <li><Link to="bitswap"><i className="fa fa-exchange"></i> Bitswap</Link></li>
-        <li><Link to="routing"><i className="fa fa-cloud"></i> Routing</Link></li>
+        <li className="hidden"><Link to="bitswap"><i className="fa fa-exchange"></i> Bitswap</Link></li>
+        <li className="hidden"><Link to="routing"><i className="fa fa-cloud"></i> Routing</Link></li>
         <li><Link to="config"><i className="fa fa-gear"></i> Config</Link></li>
         <li><Link to="logs"><i className="fa fa-list"></i> Logs</Link></li>
       </ul>
