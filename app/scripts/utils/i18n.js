18f749a4d331cdde4fae872d85271c35fc945242
dignifiedquire <dignifiedquire@gmail.com>
2015-12-23 21:52:41 +0100

    fix: Upgrade i18n libraries.
    Closes #132

--- app/scripts/utils/i18n.js
+++ app/scripts/utils/i18n.js
@@ -1,13 +1,27 @@
-var i18n = require('i18next-client')
+import i18next from 'i18next/lib'
+import XHR from 'i18next-xhr-backend/lib'
+import Cache from 'i18next-localstorage-cache/lib'
+import LanguageDetector from 'i18next-browser-languagedetector/lib'
+import sprintf from 'i18next-sprintf-postprocessor/lib'
 
-i18n.init({
-  lngWhiteList: ['en', 'cs', 'de'],
-  fallbackLng: 'en',
-  resGetPath: '/locale/webui-%{lng}.json',
-  interpolationPrefix: '%{',
-  interpolationSuffix: '}',
-  keyseparator: '<',
-  nsseparator: '>'
-})
+i18next
+  .use(XHR)
+  .use(Cache)
+  .use(LanguageDetector)
+  .use(sprintf)
+  .init({
+    lngWhiteList: ['en', 'cs', 'de'],
+    fallbackLng: 'en',
+    load: 'languageOnly',
+    backend: {
+      loadPath: '/locale/webui-{{lng}}.json',
+      allowMultiloading: false
+    },
+    interpolation: {
+      escapeValue: false
+    },
+    keyseparator: '<',
+    nsseparator: '>'
+  })
 
-module.exports = i18n
+module.exports = i18next
