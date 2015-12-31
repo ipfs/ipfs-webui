3729de2ef1c5e649afb76ca5fcbbc53f50b0b04f
dignifiedquire <dignifiedquire@gmail.com>
2015-12-31 21:38:19 +0100

    refactor: Remove unused linkhandler

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import Router from 'react-router'
 import routes from './routes'
 
-// import linkHandler from './linkhandler.js'
 if (process.env.NODE_ENV !== 'production') {
   window.uiDebug = require('debug')
 }
