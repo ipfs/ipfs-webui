bc4b3e2ca1f2f85427d0055c343ab9b3b66d6ad1
dignifiedquire <dignifiedquire@gmail.com>
2016-01-12 16:20:13 +0100

    Cleanup log data handler on unmount

--- app/scripts/pages/logs.js
+++ app/scripts/pages/logs.js
@@ -15,7 +15,8 @@ export default class Logs extends Component {
     log: [],
     tailing: true,
     nonce: 0,
-    request: null
+    request: null,
+    stream: null
   };
 
   _onLogData = chunk => {
@@ -37,6 +38,7 @@ export default class Logs extends Component {
     const request = this.props.ipfs.log.tail((err, stream) => {
       if (err) return console.error(err)
       stream.on('data', this._onLogData)
+      this.setState({stream})
     })
 
     this.setState({request})
@@ -46,6 +48,11 @@ export default class Logs extends Component {
     if (this.state.request) {
       this.state.request.destroy()
       this.setState({request: null})
+    }
+
+    if (this.state.stream) {
+      this.state.stream.removeAllListeners('data')
+      this.setState({stream: null})
     }
   }
 
