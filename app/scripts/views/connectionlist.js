a8060071931eb110ddfb766c937ed9045487a790
dignifiedquire <dignifiedquire@gmail.com>
2016-01-20 22:58:33 +0100

    feat(connections): Move the globe outside
    As discussed in #213 this removes the globe from the
    connections window and replaces it with a link.
    In addition the peers list is transformed into a table.
    
    Closes #213

--- app/scripts/views/connectionlist.js
+++ app/scripts/views/connectionlist.js
@@ -1,21 +1,32 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
+import {Table} from 'react-bootstrap'
+
 import Connection from './connection'
 
 export
 default class ConnectionList extends Component {
   static displayName = 'ConnectionList';
   static propTypes = {
-    peers: React.PropTypes.array
+    peers: PropTypes.array.isRequired
   };
-  render () {
-    const peers = this.props.peers || []
 
+  render () {
     return (
-        <ul className='list-group'>
-            {peers.map((peer, i) => {
-              return <Connection {...peer} key={i} />
-            })}
-        </ul>
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Network Address</th>
+            <th>Agent</th>
+            <th>Protocol</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.peers.map((peer, i) => {
+            return <Connection {...peer} key={i} />
+          })}
+        </tbody>
+      </Table>
     )
   }
 }
