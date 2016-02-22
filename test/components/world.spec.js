fcf6bd11c7bda89127b0dffcfc5575b26100fcff
David Mai <david.mai@isocket.com>
2016-02-21 23:54:00 -0800

    world spec

---
+++ test/components/world.spec.js
@@ -0,0 +1,22 @@
+import {expect} from 'chai'
+import {shallow} from 'enzyme'
+import React from 'react'
+
+import World from '../../app/scripts/components/world'
+
+describe('World', () => {
+  it('should have the correct defaults', () => {
+    const el = shallow(<World/>)
+    expect(el.find('AutoSizer').length).to.equal(1)
+
+    var inst = el.instance()
+    expect(inst.props.peersCount).to.equal(0)
+    expect(Object.keys(inst.props.locations).length)
+      .to.equal(0)
+  })
+
+  it('should set peersCount', () => {
+    const el = shallow(<World peersCount={1337}/>)
+    expect(el.find('.counter').node.props.children).to.equal(1337)
+  })
+})
