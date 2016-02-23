af730f5fa910b8ce4455325b98743032e384fb18
David Mai <david.mai@isocket.com>
2016-02-23 00:04:47 -0800

    assert correct coordinates

--- test/components/world-map.spec.js
+++ test/components/world-map.spec.js
@@ -1,14 +1,24 @@
 import {expect} from 'chai'
-import {render} from 'enzyme'
+import {shallow, render} from 'enzyme'
 import React from 'react'
 
 import WorldMap from '../../app/scripts/components/world-map'
 
 describe('WorldMap', () => {
-  it('should render an svg', () => {
-    const c = [[12, 14]]
+  const c = [[12, 14]]
 
+  it('should render an svg', () => {
     const el = render(<WorldMap coordinates={c}/>)
     expect(el.find('svg').length).to.equal(1)
+  })
+
+  it('should have the correct coordiates', () => {
+    const el = shallow(<WorldMap coordinates={c}/>)
+    const p = el.instance().props
+
+    c.forEach((coordinates, i) => {
+      expect(coordinates[0]).to.equal(p[i][0])
+      expect(coordinates[1]).to.equal(p[i][1])
+    })
   })
 })
