117c1ddfea552cbee65d19a4c3c0e380d495d946
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2017-01-31 17:41:27 +0100

    test: lint happy

--- test/views/object/object-link.spec.js
+++ test/views/object/object-link.spec.js
@@ -18,7 +18,7 @@ describe('ObjectLink', () => {
 
   it('renders links without a name', () => {
     const path = parse('/ipfs/hello/world')
-    const el = shallow(<ObjectLink path={path} link={{multihash:  mh.fromB58String('QmTm8QZmdYFKrT6EWBA7tfME2GXAdzkSkm7wtCZ2FqZVfT'), size: 2}} />)
+    const el = shallow(<ObjectLink path={path} link={{multihash: mh.fromB58String('QmTm8QZmdYFKrT6EWBA7tfME2GXAdzkSkm7wtCZ2FqZVfT'), size: 2}} />)
 
     expect(el.find('Link').at(0)).to.have.prop('to', 'objects/\\ipfs\\QmTm8QZmdYFKrT6EWBA7tfME2GXAdzkSkm7wtCZ2FqZVfT')
   })
