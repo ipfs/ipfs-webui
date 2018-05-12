8f9ec7cd41c5a6b327b0a2e485c79aaa0ebf3d3e
Oli Evans <oli@tableflip.io>
2018-05-12 13:57:12 +0100

    feat: add icons as react components
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

---
+++ src/icons/GlyphFolder.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GlyphFolder = props => (
+  <svg viewBox='0 0 100 100' {...props}>
+    <path d='M71.62 32.81H53.24a5.35 5.35 0 0 1-4.86-3.16L46 24.49a5 5 0 0 0-4.6-3.15h-13c-5.95 0-10.82 5.16-10.82 11.47v34.38c0 6.31 4.87 11.47 10.82 11.47h43.22c5.95 0 10.82-5.16 10.82-11.47V44.27c0-6.27-4.87-11.46-10.82-11.46z' />
+  </svg>
+)
+
+export default GlyphFolder
--- src/navigation/NavBar.js
+++ src/navigation/NavBar.js
@@ -1,29 +1,51 @@
 import React from 'react'
 import ipfsLogo from './ipfs-logo.svg'
+import StrokeMarketing from '../icons/StrokeMarketing'
+import StrokeWeb from '../icons/StrokeWeb'
+import StrokeCube from '../icons/StrokeCube'
+import StrokeSettings from '../icons/StrokeSettings'
+import StrokeDecentralization from '../icons/StrokeDecentralization'
 
 const NavLink = ({
   to,
+  icon,
   children,
-  className = 'db pv4 white tc link focus-outline',
-  activeClassName = 'bg-navy-muted'
+  className = 'dt dt--fixed pv3 mb2 white link focus-outline f5 hover-bg-white-10',
+  activeClassName = 'bg-white-10'
 }) => {
+  const Svg = icon
   const {hash} = window.location
   const href = `#${to}`
-  const cls = hash === href ? className + ' ' + activeClassName : className
-  return <a href={href} className={cls} role='menuitem'>{children}</a>
+  const active = hash === href
+  const cls = active ? className + ' ' + activeClassName : className
+  return (
+    <a href={href} className={cls} role='menuitem'>
+      <span className='dtc v-mid tr' style={{width: 100}}>
+        <Svg
+          style={{
+            width: 50,
+            opacity: active ? 1 : 0.5
+          }}
+          className='fill-current-color' />
+      </span>
+      <span className='dtc v-mid pl3'>
+        {children}
+      </span>
+    </a>
+  )
 }
 
 export default () => (
-  <div className='white pv3'>
-    <a href='#/' className='db'>
+  <div>
+    <a href='#/' className='db' style={{paddingTop: 35}}>
       <img className='db center' style={{height: 70}} src={ipfsLogo} alt='IPFS' />
     </a>
     <nav className='db pt4' role='menubar'>
-      <NavLink to='/'>Status</NavLink>
-      <NavLink to='/files'>Files</NavLink>
-      <NavLink to='/ipld'>IPLD</NavLink>
-      <NavLink to='/peers'>Peers</NavLink>
-      <NavLink to='/settings'>Settings</NavLink>
+      <NavLink to='/' icon={StrokeMarketing}>Status</NavLink>
+      <NavLink to='/files' icon={StrokeWeb}>Files</NavLink>
+      <NavLink to='/ipld' icon={StrokeDecentralization}>IPLD</NavLink>
+      <NavLink to='/peers' icon={StrokeCube}>Peers</NavLink>
+      <NavLink to='/settings' icon={StrokeSettings}>Settings</NavLink>
     </nav>
   </div>
 )
