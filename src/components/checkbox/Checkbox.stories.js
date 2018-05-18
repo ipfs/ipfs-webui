4b23f89e70980225fca18c4e9e475b9c2235694f
Henrique Dias <hacdias@gmail.com>
2018-05-18 08:58:27 +0100

    feat: checkbox

--- src/components/checkbox/Checkbox.stories.js
+++ src/components/checkbox/Checkbox.stories.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import { checkA11y } from '@storybook/addon-a11y'
 import Checkbox from './Checkbox'
+import { checkA11y } from '@storybook/addon-a11y'
 
 const bigPicture = {
   transform: 'scale(5)',
@@ -13,11 +13,11 @@ storiesOf('Checkbox', module)
   .addDecorator(checkA11y)
   .add('Default', () => (
     <div>
-      <Checkbox className='ma2' label='Click me!' onChange={action('Checked')} />
+      <Checkbox className='ma2' onChange={action('Checked')} />
     </div>
   ))
   .add('Big', () => (
     <div>
-      <Checkbox style={bigPicture} label='Click me!' className='ma2' onChange={action('Checked')} />
+      <Checkbox style={bigPicture} className='ma2' onChange={action('Checked')} />
     </div>
   ))
