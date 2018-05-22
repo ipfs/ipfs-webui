446c30f2e9cfb80b898ae2702da1aa2192e1a5ac
Henrique Dias <hacdias@gmail.com>
2018-05-22 16:05:04 +0100

    add fixture for files list

--- src/files/files-list/FilesList.stories.js
+++ src/files/files-list/FilesList.stories.js
@@ -2,40 +2,13 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import FilesList from './FilesList'
-
-const filesList = [
-  {
-    name: 'xkiahsuah.txt',
-    type: 'file',
-    size: 584451,
-    hash: 'Qm1',
-    status: 74
-  },
-  {
-    name: 'File 1.txt',
-    type: 'file',
-    size: 42452,
-    hash: 'Qm2'
-  },
-  {
-    name: 'A folder',
-    type: 'directory',
-    size: 54528448,
-    hash: 'Qm3'
-  },
-  {
-    name: 'Lorem ipsum.docx',
-    type: 'file',
-    size: 12425,
-    hash: 'Qm4'
-  }
-]
+import fixture from './fixtures/root.json'
 
 storiesOf('Files List', module)
   .add('Default', () => (
     <div className='ma2'>
       <FilesList
-        files={filesList}
+        files={fixture}
         onShare={action('Share')}
         onInspect={action('Inspect')}
         onRename={action('Rename')}
