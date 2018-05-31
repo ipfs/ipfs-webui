28878bfe02a866e423e7e3c508390ef1fc1c7344
Henrique Dias <hacdias@gmail.com>
2018-05-31 18:37:28 +0100

    make extenstions to type a map

--- src/files/type-from-ext/index.js
+++ src/files/type-from-ext/index.js
@@ -1,26 +1,7 @@
 import fileExtension from 'file-extension'
-import audioExtensions from './extensions/audio.json'
-import calcExtensions from './extensions/calc.json'
-import imageExtensions from './extensions/image.json'
-import videoExtensions from './extensions/video.json'
-import textExtensions from './extensions/text.json'
+import extToType from './extToType'
 
-export default function FileIcon (filename) {
+export default function (filename) {
   const ext = fileExtension(filename)
-
-  if (ext === 'pdf') {
-    return 'pdf'
-  } else if (audioExtensions.includes(ext)) {
-    return 'audio'
-  } else if (calcExtensions.includes(ext)) {
-    return 'calc'
-  } else if (imageExtensions.includes(ext)) {
-    return 'image'
-  } else if (videoExtensions.includes(ext)) {
-    return 'video'
-  } else if (textExtensions.includes(ext)) {
-    return 'text'
-  }
-
-  return 'blob'
+  return extToType[ext] || ext
 }
