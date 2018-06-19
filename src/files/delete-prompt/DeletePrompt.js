1e045a9ed21e0c90f88da7d889874b38d5cf5c54
Henrique Dias <hacdias@gmail.com>
2018-06-19 11:18:48 +0100

    Update DeletePrompt.js

--- src/files/delete-prompt/DeletePrompt.js
+++ src/files/delete-prompt/DeletePrompt.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'w
+import PropTypes from 'prop-types'
 import TrashIcon from '../../icons/StrokeTrash'
 import CancelIcon from '../../icons/GlyphSmallCancel'
 import Button from '../../components/button/Button'
