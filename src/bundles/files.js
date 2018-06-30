6546c261c030636961a907ffe25d5f685b02fb87
Henrique Dias <hacdias@gmail.com>
2018-06-30 09:14:45 +0100

    fix: navigation for encoded names (#693)

--- src/bundles/files.js
+++ src/bundles/files.js
@@ -13,8 +13,6 @@ const bundle = createAsyncResourceBundle({
       return Promise.resolve()
     }
 
-    // FIX: dirs with actually encoded names
-    // will get decoded... it shouldn't happen
     path = decodeURIComponent(path)
 
     return getIpfs().files.stat(path)
