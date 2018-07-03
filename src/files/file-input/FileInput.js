fe048c4c41b8462d554ed9693be27214fafe5811
Henrique Dias <hacdias@gmail.com>
2018-07-03 19:14:36 +0100

    make dropdown more stylish

--- src/files/file-input/FileInput.js
+++ src/files/file-input/FileInput.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Button from '../../components/button/Button'
-
+import DocumentIcon from '../../icons/StrokeDocument'
+import FolderIcon from '../../icons/StrokeFolder'
 import {Dropdown, DropdownMenu} from '@tableflip/react-dropdown'
 
 export default class FileInput extends React.Component {
@@ -33,9 +34,15 @@ export default class FileInput extends React.Component {
         <Dropdown>
           <Button className='f7' onClick={this.toggleOpen}>+ Add to IPFS</Button>
           <DropdownMenu open={this.state.open} width={200} onDismiss={this.toggleOpen} alignRight className='bg-aqua-muted' >
-            <nav className='flex flex-column pa3'>
-              <a className='pointer' onClick={() => this.filesInput.click()}>Add file</a>
-              <a className='pointer' onClick={() => this.folderInput.click()}>Add folder</a>
+            <nav className='flex flex-column pa2'>
+              <a className='dim ma2 pointer flex items-center' onClick={() => this.filesInput.click()}>
+                <DocumentIcon className='fill-aqua w2 mr1' />
+                Add file
+              </a>
+              <a className='dim ma2 pointer flex items-center' onClick={() => this.folderInput.click()}>
+                <FolderIcon className='fill-aqua w2 mr1' />
+                Add folder
+              </a>
             </nav>
           </DropdownMenu>
         </Dropdown>
