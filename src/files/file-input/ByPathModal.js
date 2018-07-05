d7d8cb505a4e7ba4c544a53cee8ad4d4e010e8cd
Henrique Dias <hacdias@gmail.com>
2018-07-05 09:46:47 +0100

    make the component functional

--- src/files/file-input/ByPathModal.js
+++ src/files/file-input/ByPathModal.js
@@ -1,33 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Icon from '../../icons/StrokeDecentralization'
 import isIPFS from 'is-ipfs'
-
+import Icon from '../../icons/StrokeDecentralization'
 import TextInputModal from '../text-input-modal/TextInputModal'
 
-class ByPathModal extends React.Component {
-  static propTypes = {
-    onCancel: PropTypes.func.isRequired,
-    onSubmit: PropTypes.func.isRequired
-  }
+function ByPathModal ({ onCancel, onSubmit, className, ...props }) {
+  return (
+    <TextInputModal
+      validate={isIPFS.ipfsPath}
+      onCancel={onCancel}
+      onSubmit={onSubmit}
+      className={className}
+      title='Add by path'
+      description='Insert the path to add.'
+      icon={Icon}
+      submitText='Add'
+      {...props}
+    />
+  )
+}
 
-  render () {
-    let {onCancel, onSubmit, className, ...props} = this.props
-
-    return (
-      <TextInputModal
-        validate={isIPFS.ipfsPath}
-        onCancel={onCancel}
-        onSubmit={onSubmit}
-        className={className}
-        title='Add by path'
-        description='Insert the path to add.'
-        icon={Icon}
-        submitText='Add'
-        {...props}
-      />
-    )
-  }
+ByPathModal.propTypes = {
+  onCancel: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired
 }
 
 export default ByPathModal
