07ae87c63113cd8c36856d8f3dd6cfdd8a7a2b2d
Henrique Dias <hacdias@gmail.com>
2018-07-17 15:00:17 +0100

    turn style into a constant

--- src/files/file-icon/FileIcon.js
+++ src/files/file-icon/FileIcon.js
@@ -9,23 +9,25 @@ import DocMusic from '../../icons/GlyphDocMusic'
 import DocPicture from '../../icons/GlyphDocPicture'
 import DocText from '../../icons/GlyphDocText'
 
+const style = {width: '2.5rem'}
+
 export default function FileIcon ({name, type}) {
   if (type === 'directory') {
-    return <Folder className=' fill-aqua' style={{width: '2.5rem'}} />
+    return <Folder className=' fill-aqua' style={style} />
   }
 
   switch (typeFromExt(name)) {
     case 'audio':
-      return <DocMusic className='fill-aqua' style={{width: '2.5rem'}} />
+      return <DocMusic className='fill-aqua' style={style} />
     case 'calc':
-      return <DocCalc className='fill-aqua' style={{width: '2.5rem'}} />
+      return <DocCalc className='fill-aqua' style={style} />
     case 'video':
-      return <DocMovie className='fill-aqua' style={{width: '2.5rem'}} />
+      return <DocMovie className='fill-aqua' style={style} />
     case 'text':
-      return <DocText className='fill-aqua' style={{width: '2.5rem'}} />
+      return <DocText className='fill-aqua' style={style} />
     case 'image':
-      return <DocPicture className='fill-aqua' style={{width: '2.5rem'}} />
+      return <DocPicture className='fill-aqua' style={style} />
     default:
-      return <Doc className='fill-aqua' style={{width: '2.5rem'}} />
+      return <Doc className='fill-aqua' style={style} />
   }
 }
