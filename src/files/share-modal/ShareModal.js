4cc9fc4552867c58727c8bb82acc6c7635a90150
Henrique Dias <hacdias@gmail.com>
2018-08-01 13:43:21 +0100

    fix linting
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/files/share-modal/ShareModal.js
+++ src/files/share-modal/ShareModal.js
@@ -5,34 +5,31 @@ import Button from '../../components/button/Button'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { Modal, ModalActions, ModalBody } from '../../components/modal/Modal'
 
-const ShareModal = ({onLeave, link, className, ...props}) => {
+const ShareModal = ({onLeave, link, className, ...props}) => (
+  <Modal {...props} className={className} onCancel={onLeave} >
+    <ModalBody title='Share Files' icon={ShareIcon}>
+      <p className='gray w-80 center'>
+        Copy the link below and share it with your friends.
+      </p>
 
-  return (
-    <Modal {...props} className={className} onCancel={onLeave} >
-      <ModalBody title='Share Files' icon={ShareIcon}>
-        <p className='gray w-80 center'>
-          Copy the link below and share it with your friends.
-        </p>
+      <div className='flex center w-100 pa2'>
+        <input
+          value={link}
+          readOnly
+          autoFocus
+          className={`input-reset flex-grow-1 charcoal-muted ba b--black-20 pa2 mr2 focus-outline`}
+          type='text' />
+      </div>
+    </ModalBody>
 
-        <div className='flex center w-100 pa2'>
-          <input
-            value={link}
-            readOnly
-            autoFocus
-            className={`input-reset flex-grow-1 charcoal-muted ba b--black-20 pa2 mr2 focus-outline`}
-            type='text' />
-        </div>
-      </ModalBody>
-
-      <ModalActions>
-        <Button className='ma2' bg='bg-gray' onClick={onLeave}>Close</Button>
-        <CopyToClipboard text={link} onCopy={onLeave}>
-          <Button className='ma2'>Copy</Button>
-        </CopyToClipboard>
-      </ModalActions>
-    </Modal>
-  )
-}
+    <ModalActions>
+      <Button className='ma2' bg='bg-gray' onClick={onLeave}>Close</Button>
+      <CopyToClipboard text={link} onCopy={onLeave}>
+        <Button className='ma2'>Copy</Button>
+      </CopyToClipboard>
+    </ModalActions>
+  </Modal>
+)
 
 ShareModal.propTypes = {
   onLeave: PropTypes.func.isRequired,
