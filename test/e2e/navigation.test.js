6a0f41dd6ff1caadef44c3e9a9c3910988415239
Henrique Dias <hacdias@gmail.com>
2018-08-21 22:53:05 +0100

    improve tests
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- test/e2e/navigation.test.js
+++ test/e2e/navigation.test.js
@@ -1,38 +1,29 @@
 /* global it expect beforeAll afterAll */
 import ms from 'milliseconds'
 import { launch, appUrl } from './puppeteer'
-import IPFS from 'ipfs'
 
 let browser
-let node
 
-beforeAll(done => {
-  node = new IPFS()
-
-  node.on('ready', async () => {
-    browser = await launch()
-    done()
-  })
-})
-
-afterAll(async () => {
-  browser.close()
-  await node.stop()
-})
+beforeAll(async () => { browser = await launch() })
+afterAll(() => browser.close())
 
 it('Navigation test: node not running', async () => {
-  const page = (await browser.pages())[0]
+  const page = await browser.newPage()
   await page.goto(appUrl)
   page.waitForFunction(`document.title === 'Welcome to IPFS'`, {timeout: 5000})
 }, ms.minutes(1))
 
 it('Navigation test: node running', async () => {
-  // Save 3s per run by using the initial page rather than creating a new one!
-  // const page = await browser.newPage()
-  const page = (await browser.pages())[0]
+  const page = await browser.newPage()
 
-  await page.evaluate(() => {
-    window.ipfs = node
+  await page.evaluateOnNewDocument(() => {
+    window.ipfs = {
+      id: () => {},
+      files: {
+        ls: () => [],
+        stat: () => {}
+      }
+    }
   })
 
   const waitForTitle = title => page.waitForFunction(`document.title === '${title}'`, {timeout: 5000})
@@ -40,14 +31,14 @@ it('Navigation test: node running', async () => {
   await page.goto(appUrl)
   await waitForTitle('Status - IPFS')
 
-  await page.click('nav a[href="#/files/"]')
-  await waitForTitle('Files - IPFS')
-
   await page.click('nav a[href="#/explore"]')
   await waitForTitle('Explore - IPFS')
 
   await page.click('nav a[href="#/peers"]')
   await waitForTitle('Peers - IPFS')
+
+  await page.click('nav a[href="#/files/"]')
+  await waitForTitle('Files - IPFS') // fails here?
 
   // No settings tab if IPFS is not available.
   const settingsLink = await page.$('nav a[href="#/settings"]')
@@ -55,4 +46,4 @@ it('Navigation test: node running', async () => {
 
   await page.click('nav a[href="#/"]')
   await waitForTitle('Status - IPFS')
-})
+}, ms.minutes(1))
