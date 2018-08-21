41dc89bf3018be98ac6f5ce4784471c6fe20c9b5
Henrique Dias <hacdias@gmail.com>
2018-08-21 12:19:30 +0100

    run node on tests
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- test/e2e/navigation.test.js
+++ test/e2e/navigation.test.js
@@ -1,16 +1,39 @@
 /* global it expect beforeAll afterAll */
 import ms from 'milliseconds'
 import { launch, appUrl } from './puppeteer'
+import IPFS from 'ipfs'
 
 let browser
+let node
 
-beforeAll(async () => { browser = await launch() })
-afterAll(() => browser.close())
+beforeAll(done => {
+  node = new IPFS()
 
-it('Navigation test', async () => {
+  node.on('ready', async () => {
+    browser = await launch()
+    done()
+  })
+})
+
+afterAll(async () => {
+  browser.close()
+  await node.stop()
+})
+
+it('Navigation test: node not running', async () => {
+  const page = (await browser.pages())[0]
+  await page.goto(appUrl)
+  page.waitForFunction(`document.title === 'Welcome to IPFS'`, {timeout: 5000})
+}, ms.minutes(1))
+
+it('Navigation test: node running', async () => {
   // Save 3s per run by using the initial page rather than creating a new one!
   // const page = await browser.newPage()
   const page = (await browser.pages())[0]
+
+  await page.evaluate(() => {
+    window.ipfs = node
+  })
 
   const waitForTitle = title => page.waitForFunction(`document.title === '${title}'`, {timeout: 5000})
 
@@ -32,4 +55,4 @@ it('Navigation test', async () => {
 
   await page.click('nav a[href="#/"]')
   await waitForTitle('Status - IPFS')
-}, ms.minutes(1))
+})
