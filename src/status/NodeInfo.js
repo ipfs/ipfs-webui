d8ba78f7c6244fc96513d0a9c847415f83e0c341
Henrique Dias <hacdias@gmail.com>
2018-08-24 17:27:56 +0100

    some node info tweaks
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/status/NodeInfo.js
+++ src/status/NodeInfo.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react'
 import { connect } from 'redux-bundler-react'
 import Box from '../components/box/Box'
 
+const Block = ({label, value}) => (
+  <div className='dt dt--fixed pt2'>
+    <label className='dtc silver tracked ttu f7' style={{width: '100px'}}>{label}</label>
+    <div className='dtc truncate charcoal monospace'>{value}</div>
+  </div>
+)
+
 export class NodeInfo extends Component {
   render () {
     const {ipfsIdentity} = this.props
     console.log(this.props)
     return (
       <Box>
-        <pre>
-          { JSON.stringify(ipfsIdentity, null, 2) }
-        </pre>
+        <h2 className='dib tracked ttu f6 fw2 teal-muted hover-aqua link mt0 mb2'>Node Info</h2>
+
+        <div className='f6'>
+          <Block label='CID' value={ipfsIdentity.id} />
+          <Block label='Public Key' value={ipfsIdentity.publicKey} />
+        </div>
       </Box>
     )
   }
