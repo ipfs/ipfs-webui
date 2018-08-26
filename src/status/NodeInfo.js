ddd1985191854a438189d9b845d6a137b94818ed
Henrique Dias <hacdias@gmail.com>
2018-08-26 11:23:52 +0100

    better alignment
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/status/NodeInfo.js
+++ src/status/NodeInfo.js
@@ -14,14 +14,14 @@ export function NodeInfo ({ ipfsIdentity, peers }) {
     <Box>
       <h2 className='dib tracked ttu f6 fw2 teal-muted hover-aqua link mt0 mb2'>Node Info</h2>
 
-      <div className='f6 flex'>
-        <div className='w-50 mr3'>
+      <div className='f6 flex flex-column flex-row-l'>
+        <div className='w-100 w-50-l pr2-l'>
           <Block label='CID' value={ipfsIdentity.id} />
           <Block label='Peers' value={peers ? peers.length : 0} />
           <Block label='Version' value={ipfsIdentity.agentVersion} />
         </div>
-        <div className='w-50 ml3'>
-          Right side
+        <div className='w-100 w-50-l pl2-l mt3 mt0-l'>
+          Right Side
         </div>
       </div>
     </Box>
