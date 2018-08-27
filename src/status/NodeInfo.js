2d410770af3db5a43504473509dbbb68421f32a8
Henrique Dias <hacdias@gmail.com>
2018-08-27 08:35:55 +0100

    advanced info on NodeInfo
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/status/NodeInfo.js
+++ src/status/NodeInfo.js
@@ -1,31 +1,91 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'redux-bundler-react'
 import Box from '../components/box/Box'
 
-const Block = ({label, value}) => (
+const Block = ({ children }) => (
   <div className='dt dt--fixed pt2'>
-    <label className='dtc silver tracked ttu f7' style={{width: '100px'}}>{label}</label>
-    <div className='dtc truncate charcoal monospace'>{value}</div>
+    { children }
   </div>
 )
 
-export function NodeInfo ({ ipfsIdentity, peers }) {
-  return (
-    <Box>
-      <h2 className='dib tracked ttu f6 fw2 teal-muted hover-aqua link mt0 mb2'>Node Info</h2>
+const Label = ({ children }) => (
+  <label className='dtc silver tracked ttu f7' style={{width: '100px'}}>{children}</label>
+)
 
-      <div className='f6 flex flex-column flex-row-l'>
-        <div className='w-100 w-50-l pr2-l'>
-          <Block label='CID' value={ipfsIdentity.id} />
-          <Block label='Peers' value={peers ? peers.length : 0} />
-          <Block label='Version' value={ipfsIdentity.agentVersion} />
+const Value = ({ children, wrap = false }) => (
+  <div className={`dtc charcoal monospace ${wrap ? 'word-wrap' : 'truncate'}`}>{children}</div>
+)
+
+class NodeInfo extends React.Component {
+  static propTypes = {
+    ipfsIdentity: PropTypes.object.isRequired,
+    peers: PropTypes.array.isRequired
+  }
+
+  state = {
+    advanced: false
+  }
+
+  toggleAdvanced = () => {
+    this.setState(s => ({ advanced: !s.advanced }))
+  }
+
+  render () {
+    const { ipfsIdentity, peers } = this.props
+
+    return (
+      <Box className='f6 pa4'>
+        <h2 className='dib tracked ttu f6 fw2 teal-muted hover-aqua link mt0 mb2'>Node Info</h2>
+
+        <div className='flex flex-column flex-row-l'>
+          <div className='w-100 w-50-l pr2-l'>
+            <Block>
+              <Label>CID</Label>
+              <Value>{ipfsIdentity.id}</Value>
+            </Block>
+            <Block>
+              <Label>Peers</Label>
+              <Value>{peers ? peers.length : 0}</Value>
+            </Block>
+            <Block>
+              <Label>Version</Label>
+              <Value>{ipfsIdentity.agentVersion}</Value>
+            </Block>
+          </div>
+          <div className='w-100 w-50-l pl2-l mt3 mt0-l'>
+            Right Side
+          </div>
         </div>
-        <div className='w-100 w-50-l pl2-l mt3 mt0-l'>
-          Right Side
+
+        <div className='mt3'>
+          <span className='pointer monospace' onClick={this.toggleAdvanced}>
+            {this.state.advanced ? 'Hide' : 'Show'} advanced
+            <span className='ml1 dib transition-all' style={this.state.advanced ? {transform: 'rotate(-180deg)'} : null}>▼</span>
+          </span>
         </div>
-      </div>
-    </Box>
-  )
+
+        <div className='transition-all overflow-hidden' style={
+          this.state.advanced ? {
+            height: 'auto',
+            marginTop: '1rem'
+          } : {
+            height: '0',
+            marginTop: '0'
+          }
+        }>
+          <Block>
+            <Label>Public Key</Label>
+            <Value wrap>{ipfsIdentity.publicKey}</Value>
+          </Block>
+          <Block>
+            <Label>Addresses</Label>
+            <Value wrap>{ipfsIdentity.addresses.map(addr => <div>{addr}</div>)}</Value>
+          </Block>
+        </div>
+      </Box>
+    )
+  }
 }
 
 export default connect(
