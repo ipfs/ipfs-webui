0e77fc5d4c17766cd5b259a117978fb329b9fa06
Oli Evans <oli@tableflip.io>
2018-08-29 15:31:33 +0100

    chore: reuse ipld-explorer as git submodule
    This is the shortest path to re-using the ipld explorer code
    in webui. Publishing ipld explorer as a reuseable module needs
    more thought, and right now we want to be able to build and
    edit all of the webui code from a single code base, while we
    do the polishing for release.
    
    In the future ipld explorer may become one app in a learna repo
    for all the webui sections, or we might focus on a post-message
    api to co-ordinate ipfs apps via iframes with webui orchestrating
    things and providing the ipfs instance to child frames.
    
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- src/bundles/routes.js
+++ src/bundles/routes.js
@@ -1,13 +1,15 @@
 import { createRouteBundle } from 'redux-bundler'
 import StatusPage from '../status/LoadableStatusPage'
 import FilesPage from '../files/LoadableFilesPage'
-import ExplorePage from '../explore/LoadableExplorePage'
+import ExplorePage from '../explore/src/components/LoadableExplorePage'
+import StartExploringPage from '../explore/src/components/StartExploringPage'
 import PeersPage from '../peers/LoadablePeersPage'
 import SettingsPage from '../settings/LoadableSettingsPage'
 import WelcomePage from '../welcome/LoadableWelcomePage'
 
 export default createRouteBundle({
-  '/explore*': ExplorePage,
+  '/explore/*': ExplorePage,
+  '/explore': StartExploringPage,
   '/files*': FilesPage,
   '/peers': PeersPage,
   '/settings': SettingsPage,
--- src/explore/LoadableExplorePage.js
+++
@@ -1,9 +0,0 @@
-import Loadable from 'react-loadable'
-import ComponentLoader from '../loader/ComponentLoader.js'
-
-const LoadableExplorePage = Loadable({
-  loader: () => import('./ExplorePage'),
-  loading: ComponentLoader
-})
-
-export default LoadableExplorePage
