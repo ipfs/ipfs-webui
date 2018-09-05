ef2a7d168dd603c7acb2007e6357eadea5c78393
Oli Evans <oli@tableflip.io>
2018-09-05 17:09:55 +0100

    chore: use ipld-explorer-components
    Reuse the ipld-explorer code.
    
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- src/bundles/routes.js
+++ src/bundles/routes.js
@@ -1,12 +1,13 @@
 import { createRouteBundle } from 'redux-bundler'
 import StatusPage from '../status/LoadableStatusPage'
 import FilesPage from '../files/LoadableFilesPage'
-import ExplorePage from '../explore/LoadableExplorePage'
+import { ExplorePage, StartExploringPage } from 'ipld-explorer-components'
 import PeersPage from '../peers/LoadablePeersPage'
 import SettingsPage from '../settings/LoadableSettingsPage'
 import WelcomePage from '../welcome/LoadableWelcomePage'
 
 export default createRouteBundle({
+  '/explore': StartExploringPage,
   '/explore*': ExplorePage,
   '/files*': FilesPage,
   '/peers': PeersPage,
