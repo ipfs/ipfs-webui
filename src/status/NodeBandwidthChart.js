9cafe5177105bc5dd75f68a5b31ffefd4252ca71
Henrique Dias <hacdias@gmail.com>
2018-09-05 14:08:39 +0100

    wip: node bandwidth tooltip
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/status/NodeBandwidthChart.js
+++ src/status/NodeBandwidthChart.js
@@ -7,7 +7,33 @@ import filesize from 'filesize'
 import { Title } from './Commons'
 import Box from '../components/box/Box'
 
-const humansize = filesize.partial({ round: 1, bits: true })
+const humansize = filesize.partial({ round: 0, bits: true })
+
+const Tooltip = ({ bw, show, pos }) => {
+  if (!show) {
+    return null
+  }
+
+  bw = {
+    in: filesize(bw.in, { round: 0, bits: true, output: 'array' }),
+    out: filesize(bw.out, { round: 0, bits: true, output: 'array' })
+  }
+
+  return (
+    <div id='chartjs-tooltip' className='fixed bg-white pa2 br3 shadow-4' style={{ top: `${pos.top}px`, left: `${pos.left}px` }}>
+      <div className='db'>
+        <span className='w2 dib charcoal tr'>in:</span>
+        <span className='f4 ml2 charcoal-muted'>{bw.in[0]}</span>
+        <span className='ml1 charcoal-muted'>{bw.in[1]}/s</span>
+      </div>
+      <div className='db'>
+        <span className='w2 dib charcoal tr'>out:</span>
+        <span className='f4 ml2 charcoal-muted'>{bw.out[0]}</span>
+        <span className='ml1 charcoal-muted'>{bw.out[1]}/s</span>
+      </div>
+    </div>
+  )
+}
 
 export class NodeBandwidthChart extends Component {
   static propTypes = {
@@ -15,7 +41,9 @@ export class NodeBandwidthChart extends Component {
     animatedPoints: PropTypes.number
   }
 
-  state = {}
+  state = {
+    tooltip: { show: false }
+  }
 
   static defaultProps = {
     animatedPoints: 500 // Only animate for the first 500 points
@@ -43,10 +71,38 @@ export class NodeBandwidthChart extends Component {
       }
     ]
 
+    const updateTooltip = (show, bw, pos) => {
+      this.setState({
+        tooltip: { show, bw, pos }
+      })
+    }
+
     const options = {
       defaultFontFamily: "'Inter UI', system-ui, sans-serif",
       responsive: true,
-      tooltips: { mode: 'index' },
+      tooltips: {
+        mode: 'index',
+        enabled: false,
+        custom: function (model) {
+          // FIX: this is NOT HAPPENING!?!?!?!
+          if (model.opacity === 0) {
+            updateTooltip(false)
+            return
+          }
+
+          const lines = model.body
+            .map(i => i.lines)
+            .map(l => l[0].split(' ')[1])
+            .map(n => parseInt(n, 10))
+
+          const pos = this._chart.canvas.getBoundingClientRect()
+
+          updateTooltip(true, { in: lines[0], out: lines[1] }, {
+            left: pos.left + model.caretX,
+            top: pos.top + model.caretY
+          })
+        }
+      },
       hover: { mode: 'index' },
       scales: {
         xAxes: [{ type: 'time' }],
@@ -71,6 +127,7 @@ export class NodeBandwidthChart extends Component {
     return (
       <Box className={`pa4 pr2 ${this.props.className}`}>
         <Title>{t('bandwidthOverTime')}</Title>
+        <Tooltip {...this.state.tooltip} />
         <Line data={{ datasets }} options={options} />
       </Box>
     )
