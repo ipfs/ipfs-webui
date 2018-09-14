30da04247bf1cf0f73bf3bf9f07a1e763eca21c4
Oli Evans <oli@tableflip.io>
2018-09-14 17:38:22 +0100

    fix: add force-select to address for easier ip grabbing
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- src/components/address/Address.js
+++ src/components/address/Address.js
@@ -5,8 +5,8 @@ const Address = ({ value }) => {
   return (
     <div className='charcoal-muted pb1 monospace'>
       {parts.map((chunk, i) => (
-        <span>
-          <span className={i % 2 || i > 4 ? '' : 'charcoal'}>{chunk}</span>
+        <span key={i}>
+          <span className={i % 2 || i > 4 ? 'force-select' : 'force-select charcoal'}>{chunk}</span>
           {i < parts.length - 1 ? '/' : ''}
         </span>
       ))}
