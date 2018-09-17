89aa7d7032436412fb249f02d6bd7514177c8900
Oli Evans <oli@tableflip.io>
2018-09-17 09:21:44 +0100

    feat: set config editor height from content (#796)
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- src/settings/editor/JsonEditor.js
+++ src/settings/editor/JsonEditor.js
@@ -7,6 +7,8 @@ import './theme/ipfs_dark'
 class JsonEditor extends React.Component {
   render () {
     const { value, readOnly, onChange } = this.props
+    const lineHeight = 16
+    const height = Math.max(500, value.split('\n').length * lineHeight)
     return (
       <div className='pv3 bg-navy br2'>
         <AceEditor
@@ -16,7 +18,7 @@ class JsonEditor extends React.Component {
           mode='json'
           theme='ipfs_dark'
           width='100%'
-          height='2100px'
+          height={height + 'px'}
           fontSize={12}
           showPrintMargin={false}
           showGutter
