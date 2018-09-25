7bfadad12de0e52b7ca285b127b2cd19600a120b
Diogo Silva <fsdiogo@gmail.com>
2018-09-25 15:22:12 +0100

    fix: same border-radius across inputs

--- src/components/text-input-modal/TextInputModal.js
+++ src/components/text-input-modal/TextInputModal.js
@@ -108,7 +108,7 @@ class TextInputModal extends React.Component {
             value={this.state.value}
             required
             autoFocus
-            className={`input-reset charcoal ba b--black-20 pa2 mb2 db w-75 center focus-outline ${this.inputClass}`}
+            className={`input-reset charcoal ba b--black-20 br1 pa2 mb2 db w-75 center focus-outline ${this.inputClass}`}
             type='text' />
         </ModalBody>
 
