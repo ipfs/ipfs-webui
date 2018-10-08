93dfd6ec3142316d5c5d42f72a21b53ea50ae9b7
Oli Evans <oli@tableflip.io>
2018-10-08 10:29:01 +0100

    fix: i18n for welcome and files page
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

---
+++ src/components/about-ipfs/AboutIpfs.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { translate, Trans } from 'react-i18next'
+import Box from '../box/Box'
+
+export const AboutIpfs = ({ t }) => {
+  return (
+    <Box>
+      <h1 className='mt0 mb3 montserrat fw4 f4 charcoal'>{t('aboutIpfs.header')}</h1>
+      <Trans i18nKey='aboutIpfs.paragraph1'>
+        <p className='mt0'><strong>IPFS is a protocol</strong> that defines a content-addressed file system, coordinates content delivery and combines ideas from Kademlia, BitTorrent, Git and more.</p>
+      </Trans>
+      <Trans i18nKey='aboutIpfs.paragraph2'>
+        <p><strong>IPFS is a filesystem.</strong> It has directories and files and mountable filesystem via FUSE.</p>
+      </Trans>
+      <Trans i18nKey='aboutIpfs.paragraph3'>
+        <p><strong>IPFS is a web.</strong> iles are accessible via HTTP gateways like <code className='f7'>https://ipfs.io</code>. Browsers <a className='link blue' target='_blank' rel='noopener noreferrer' href='https://github.com/ipfs-shipyard/ipfs-companion#release-channel'>can be extended</a> to use the <code className='f7'>ipfs://</code> scheme directly, and hash-addressed content guarantees authenticity</p>
+      </Trans>
+      <Trans i18nKey='aboutIpfs.paragraph4'>
+        <p><strong>IPFS is p2p.</strong> It supports worldwide peer-to-peer file transfers with a completely decentralized architecture and no central point of failure.</p>
+      </Trans>
+      <Trans i18nKey='aboutIpfs.paragraph5'>
+        <p><strong>IPFS is a CDN.</strong> Add a file to your local repository, and it's now available to the world with cache-friendly content-hash addressing and bittorrent-like bandwidth distribution.</p>
+      </Trans>
+    </Box>
+  )
+}
+
+export default translate('welcome')(AboutIpfs)
--- src/files/FilesPage.js
+++ src/files/FilesPage.js
@@ -4,7 +4,7 @@ import { Helmet } from 'react-helmet'
 import { connect } from 'redux-bundler-react'
 import downloadFile from './download-file'
 import { join } from 'path'
-import { translate } from 'react-i18next'
+import { translate, Trans } from 'react-i18next'
 
 // Components
 import Breadcrumbs from './breadcrumbs/Breadcrumbs'
@@ -15,6 +15,7 @@ import Overlay from '../components/overlay/Overlay'
 import ShareModal from './share-modal/ShareModal'
 import RenameModal from './rename-modal/RenameModal'
 import DeleteModal from './delete-modal/DeleteModal'
+import AboutIpfs from '../components/about-ipfs/AboutIpfs'
 import Box from '../components/box/Box'
 
 const defaultState = {
@@ -226,7 +227,7 @@ class FilesPage extends React.Component {
 
             { isRoot && !filesExist && !isCompanion && <AddFilesInfo /> }
 
-            { isRoot && !filesExist && <WelcomeInfo /> }
+            { isRoot && !filesExist && <WelcomeInfo t={t} /> }
 
             { files.type === 'directory' ? (
               <FilesList
@@ -283,9 +284,11 @@ class FilesPage extends React.Component {
 }
 
 const CompanionInfo = () => (
-  <div className='mv4 tc navy f6' >
+  <div className='mv4 tc navy f5' >
     <Box style={{ background: 'rgba(105, 196, 205, 0.1)' }}>
-      <p className='ma0'>As you are using <strong>IPFS Companion</strong>, the files view is limited to files added while using the extension.</p>
+      <Trans i18nKey='companionInfo'>
+        <p className='ma0'>As you are using <strong>IPFS Companion</strong>, the files view is limited to files added while using the extension.</p>
+      </Trans>
     </Box>
   </div>
 )
@@ -293,33 +296,40 @@ const CompanionInfo = () => (
 const AddFilesInfo = () => (
   <div className='mv4 tc navy f5' >
     <Box style={{ background: 'rgba(105, 196, 205, 0.1)' }}>
-      <p className='ma0'>Add files to your local IPFS node by clicking the <strong>Add to IPFS</strong> button above.</p>
+      <Trans i18nKey='addFilesInfo'>
+        <p className='ma0'>Add files to your local IPFS node by clicking the <strong>Add to IPFS</strong> button above.</p>
+      </Trans>
     </Box>
   </div>
 )
 
-const WelcomeInfo = () => (
+const WelcomeInfo = ({ t }) => (
   <div className='flex'>
     <div className='flex-auto pr3 lh-copy mid-gray'>
       <Box>
-        <h1 className='mt0 mb4 montserrat fw4 f4 charcoal'>Welcome to the IPFS Web UI</h1>
-        <p className='f5'><a href='#/' className='link blue u b'>Check the status</a> of your node, it's Peer ID and connection info, the network traffic and the number of connected peers.</p>
-        <p className='f5'>Easily <a href='#/files' className='link blue b'>manage files</a> in your IPFS repo. You can drag and drop to add files, move and rename them, delete, share or download them.</p>
-        <p className='f5'>You can <a href='#/explore' className='link blue b'>explore IPLD data</a> that underpins how IPFS works.</p>
-        <p className='f5'>See all of your <a href='#/peers' className='link blue b'>connected peers</a>, geolocated by their IP address.</p>
-        <p className='mb4 f5'><a href='#/settings' className='link blue b'>Review the settings</a> for your IPFS node, and update them to better suit your needs.</p>
-        <p className='mb0 f5'>If you want to help push the Web UI forward, <a href='https://github.com/ipfs-shipyard/ipfs-webui' className='link blue'>check out its code</a> or <a href='https://github.com/ipfs-shipyard/ipfs-webui/issues' className='link blue'>report a bug</a>!</p>
+        <h1 className='mt0 mb3 montserrat fw4 f4 charcoal'>{t('welcomeInfo.header')}</h1>
+        <Trans i18nKey='welcomeInfo.paragraph1'>
+          <p className='f5'><a href='#/' className='link blue u b'>Check the status</a> of your node, it's Peer ID and connection info, the network traffic and the number of connected peers.</p>
+        </Trans>
+        <Trans i18nKey='welcomeInfo.paragraph2'>
+          <p className='f5'>Easily <a href='#/files' className='link blue b'>manage files</a> in your IPFS repo. You can drag and drop to add files, move and rename them, delete, share or download them.</p>
+        </Trans>
+        <Trans i18nKey='welcomeInfo.paragraph3'>
+          <p className='f5'>You can <a href='#/explore' className='link blue b'>explore IPLD data</a> that underpins how IPFS works.</p>
+        </Trans>
+        <Trans i18nKey='welcomeInfo.paragraph4'>
+          <p className='f5'>See all of your <a href='#/peers' className='link blue b'>connected peers</a>, geolocated by their IP address.</p>
+        </Trans>
+        <Trans i18nKey='welcomeInfo.paragraph5'>
+          <p className='mb4 f5'><a href='#/settings' className='link blue b'>Review the settings</a> for your IPFS node, and update them to better suit your needs.</p>
+        </Trans>
+        <Trans i18nKey='welcomeInfo.paragraph6'>
+          <p className='mb0 f5'>If you want to help push the Web UI forward, <a href='https://github.com/ipfs-shipyard/ipfs-webui' className='link blue'>check out its code</a> or <a href='https://github.com/ipfs-shipyard/ipfs-webui/issues' className='link blue'>report a bug</a>!</p>
+        </Trans>
       </Box>
     </div>
     <div className='measure lh-copy dn db-l flex-none mid-gray f6' style={{ maxWidth: '40%' }}>
-      <Box>
-        <h1 className='mt0 mb4 montserrat fw4 f4 charcoal'>What is IPFS?</h1>
-        <p className='mt0'><strong>IPFS is a protocol</strong> that defines a content-addressed file system, coordinates content delivery and combines ideas from Kademlia, BitTorrent, Git and more.</p>
-        <p><strong>IPFS is a filesystem.</strong> It has directories and files and mountable filesystem via FUSE.</p>
-        <p><strong>IPFS is a web.</strong> Files are accessible via HTTP at <code className='f6'>https://ipfs.io/&lt;path&gt;</code>. Browsers <a className='link blue' target='_blank' rel='noopener noreferrer' href='https://github.com/ipfs-shipyard/ipfs-companion#release-channel'>can be extended</a> to use the <code className='f6'>ipfs://</code> or <code className='f6'>dweb:/ipfs/</code> schemes directly, and hash-addressed content guarantees authenticity</p>
-        <p><strong>IPFS is p2p.</strong> It supports worldwide peer-to-peer file transfers with a completely decentralized architecture and no central point of failure.</p>
-        <p><strong>IPFS is a CDN.</strong> Add a file to your local repository, and it's now available to the world with cache-friendly content-hash addressing and bittorrent-like bandwidth distribution.</p>
-      </Box>
+      <AboutIpfs />
     </div>
   </div>
 )
