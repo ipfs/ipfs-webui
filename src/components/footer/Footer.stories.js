47de537cbcd1a250d966b976e580e70683bb200a
Diogo Silva <fsdiogo@gmail.com>
2018-10-18 11:19:18 +0100

    fix: storybook navbar

--- src/components/footer/Footer.stories.js
+++ src/components/footer/Footer.stories.js
@@ -4,7 +4,7 @@ import i18n from '../../i18n'
 import Footer from './Footer.js'
 
 storiesOf('Footer', module)
-  .add('default', () => (
+  .add('Default', () => (
     <div className='sans-serif' style={{ padding: 10 }}>
       <Footer codeUrl='https://github.com/ipfs-shipyard/ipld-explorer-components' t={i18n.getFixedT('en', 'status')} />
     </div>
