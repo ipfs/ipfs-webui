6f9f64c5e85d8005507a55ff32adfe928f0a8d89
Oli Evans <oli@tableflip.io>
2018-11-02 17:42:40 +0000

    feat: use local names for languages
    Introducing `lol` the locale-to-languge cli tool!
    
    https://github.com/olizilla/lol
    
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -1,6 +1,13 @@
 import i18n from '../i18n'
+// languages.json generated from our locale dir via https://github.com/olizilla/lol
 import languages from './languages.json'
 
-export const getCurrentLanguage = () => languages[i18n.language] || i18n.language
+export const getCurrentLanguage = () => {
+  return getLanguage(i18n.language)
+}
 
-export const getLanguage = (lang) => languages[lang] || lang
+export const getLanguage = (localeCode) => {
+  const info = languages[localeCode]
+  if (!info) return localeCode
+  return info.nativeName
+}
