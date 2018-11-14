be2cb715342015ad7f5045253865cd82a84e48e2
Henrique Dias <hacdias@gmail.com>
2018-11-14 08:40:28 +0000

    fix: ipfs desktop settings
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/bundles/ipfs-desktop.js
+++ src/bundles/ipfs-desktop.js
@@ -1,11 +1,18 @@
-const bundle = {
-  name: 'ipfs-desktop',
+let bundle = {
+  name: 'ipfsDesktop',
   reducer: (state = {}) => state,
-  selectIsIpfsDesktop: () => !!window.ipfsDesktop,
-  selectDesktopSettings: window.ipfsDesktop.selectDesktopSettings,
-  doDesktopStartListening: window.ipfsDesktop.doDesktopStartListening,
-  doDesktopSettingsToggle: window.ipfsDesktop.doDesktopSettingsToggle,
-  init: window.ipfsDesktop.init
+  selectIsIpfsDesktop: () => !!window.ipfsDesktop
+}
+
+if (window.ipfsDesktop) {
+  bundle = {
+    ...bundle,
+    reducer: window.ipfsDesktop.reducer,
+    selectDesktopSettings: window.ipfsDesktop.selectDesktopSettings,
+    doDesktopStartListening: window.ipfsDesktop.doDesktopStartListening,
+    doDesktopSettingsToggle: window.ipfsDesktop.doDesktopSettingsToggle,
+    init: window.ipfsDesktop.init
+  }
 }
 
 export default bundle
