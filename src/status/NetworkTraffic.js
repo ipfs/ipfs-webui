895420e042a85b312b60d398bfb4fa15a4ae8dd4
Oli Evans <oli@tableflip.io>
2018-12-13 15:04:31 +0000

    replace distribution chart with better node stats (#902)
    - remove distribution chart from status page
    - factor out Shell and Definition components
    - Adds a Shell component to show terminal info
    - Adds a Definition component to show key/value info lists
    - Show help to user when connection to the api is lost
    - Connection status & repo size on status page
    - State clearly that the user is connected to IPFS
    - Show repo size as a link to the files page
    - Show peers discovered as a link to the peers page
    - Show clearly when connection is lost and steps to fix it.
    - Refactor status page and extract out reusable components
    
    
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

---
+++ src/status/NetworkTraffic.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { connect } from 'redux-bundler-react'
+import { translate } from 'react-i18next'
+import Speedometer from './Speedometer'
+import { Title } from './Commons'
+
+class NetworkTraffic extends React.Component {
+  state = {
+    downSpeed: {
+      filled: 0,
+      total: 125000 // Starts with 1 Mb/s max
+    },
+    upSpeed: {
+      filled: 0,
+      total: 125000 // Starts with 1 Mb/s max
+    }
+  }
+
+  componentDidUpdate (_, prevState) {
+    const { stats } = this.props
+    const down = stats ? parseInt(stats.bw.rateIn.toFixed(0), 10) : 0
+    const up = stats ? parseInt(stats.bw.rateOut.toFixed(0), 10) : 0
+
+    if (down !== prevState.downSpeed.filled || up !== prevState.upSpeed.filled) {
+      this.setState({
+        downSpeed: {
+          filled: down,
+          total: Math.max(down, prevState.downSpeed.total)
+        },
+        upSpeed: {
+          filled: up,
+          total: Math.max(up, prevState.upSpeed.total)
+        }
+      })
+    }
+  }
+
+  render () {
+    const { t } = this.props
+    const { downSpeed, upSpeed } = this.state
+
+    return (
+      <div>
+        <Title>{t('networkTraffic')}</Title>
+        <div className='flex flex-column justify-between' style={{ maxWidth: 400 }}>
+          <div className='mh2 mv3 mt0-l'>
+            <Speedometer
+              title={t('upSpeed')}
+              color='#f39021'
+              {...upSpeed} />
+          </div>
+          <div className='mh2 mt3 mt0-l'>
+            <Speedometer
+              title={t('downSpeed')}
+              color='#69c4cd'
+              {...downSpeed} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default connect(
+  'selectStats',
+  translate('status')(NetworkTraffic)
+)
---
+++ src/status/NodeInfoAdvanced.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { connect } from 'redux-bundler-react'
+import { translate } from 'react-i18next'
+import Address from '../components/address/Address'
+import Details from '../components/details/Details'
+import ProviderLink from '../components/provider-link/ProviderLink'
+import { Definition, DefinitionList } from '../components/definition/Definition.js'
+
+class NodeInfoAdvanced extends React.Component {
+  getField (obj, field, fn) {
+    if (obj && obj[field]) {
+      if (fn) {
+        return fn(obj[field])
+      }
+
+      return obj[field]
+    }
+
+    return ''
+  }
+
+  render () {
+    const { t, identity, ipfsProvider, ipfsApiAddress, gatewayUrl } = this.props
+    let publicKey = null
+    let addresses = null
+    if (identity) {
+      publicKey = this.getField(identity, 'publicKey')
+      addresses = [...new Set(identity.addresses)].sort().map(addr => <Address key={addr} value={addr} />)
+    }
+    return (
+      <Details className='mt3 f6' summaryText={t('advanced')}>
+        <DefinitionList className='mt3'>
+          <Definition advanced term={t('gateway')} desc={gatewayUrl} />
+          {ipfsProvider === 'js-ipfs-api'
+            ? <Definition advanced term={t('api')} desc={<Address value={ipfsApiAddress} />} />
+            : <Definition advanced term={t('api')} desc={<ProviderLink name={ipfsProvider} />} />
+          }
+          <Definition advanced term={t('addresses')} desc={addresses} />
+          <Definition advanced term={t('publicKey')} desc={publicKey} />
+        </DefinitionList>
+      </Details>
+    )
+  }
+}
+
+export default connect(
+  'selectIdentity',
+  'selectIpfsProvider',
+  'selectIpfsApiAddress',
+  'selectGatewayUrl',
+  translate('status')(NodeInfoAdvanced)
+)
