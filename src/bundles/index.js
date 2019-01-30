16ade59b487a12b935dfc3983d3ab16c779e8473
Diogo Silva <fsdiogo@gmail.com>
2019-01-30 12:04:45 +0000

    chore: update to the latest  ipld api  (#948)
    Update ipld and ipld-explorer-components to work with the latest API.

--- src/bundles/index.js
+++ src/bundles/index.js
@@ -33,7 +33,32 @@ export default composeBundles(
   redirectsBundle,
   statsBundle,
   filesBundle(),
-  exploreBundle(() => import('ipld')),
+  exploreBundle(async () => {
+    const [
+      // IPLD
+      ipld,
+      // IPLD Formats
+      ipldBitcoin, ipldDagCbor, ipldDagPb, ipldGit, ipldRaw, ipldZcash,
+      { ethAccountSnapshot, ethBlock, ethBlockList, ethStateTrie, ethStorageTrie, ethTxTrie, ethTx }
+    ] = await Promise.all([
+      import(/* webpackChunkName: "ipld" */ 'ipld'),
+      import(/* webpackChunkName: "ipld" */ 'ipld-bitcoin'),
+      import(/* webpackChunkName: "ipld" */ 'ipld-dag-cbor'),
+      import(/* webpackChunkName: "ipld" */ 'ipld-dag-pb'),
+      import(/* webpackChunkName: "ipld" */ 'ipld-git'),
+      import(/* webpackChunkName: "ipld" */ 'ipld-raw'),
+      import(/* webpackChunkName: "ipld" */ 'ipld-zcash'),
+      import(/* webpackChunkName: "ipld" */ 'ipld-ethereum')
+    ])
+
+    return {
+      ipld: ipld,
+      formats: [
+        ipldBitcoin, ipldDagCbor, ipldDagPb, ipldGit, ipldRaw, ipldZcash,
+        ethAccountSnapshot, ethBlock, ethBlockList, ethStateTrie, ethStorageTrie, ethTxTrie, ethTx
+      ]
+    }
+  }),
   configBundle,
   configSaveBundle,
   nodeBandwidthBundle,
