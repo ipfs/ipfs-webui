1a5ffe081a5913424fa8216e2241d4ab5c71c9e6
Oli Evans <oli@tableflip.io>
2019-04-24 11:44:53 +0100

    chore: fix lint error
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- src/files/file-input/FileInput.js
+++ src/files/file-input/FileInput.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import classnames from 'classnames'
 import { translate } from 'react-i18next'
 // Icons
 import DocumentIcon from '../../icons/StrokeDocument'
