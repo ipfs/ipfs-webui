a422e9ec12fb72c1cc27381082799b2650b86c7f
Henrique Dias <hacdias@gmail.com>
2019-05-01 14:54:16 +0100

    wip: move informational boxes outside
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/files/FilesPage.js
+++ src/files/FilesPage.js
@@ -16,9 +16,10 @@ import NewFolderModal from './new-folder-modal/NewFolderModal'
 import ShareModal from './share-modal/ShareModal'
 import RenameModal from './rename-modal/RenameModal'
 import DeleteModal from './delete-modal/DeleteModal'
-import AboutIpfs from '../components/about-ipfs/AboutIpfs'
-import Box from '../components/box/Box'
 import Button from '../components/button/Button'
+import WelcomeInfo from './info/WelcomeInfo'
+import CompanionInfo from './info/CompanionInfo'
+import AddFilesInfo from './info/AddFilesInfo'
 // Icons
 import FolderIcon from '../icons/StrokeFolder'
 
@@ -326,57 +327,6 @@ class FilesPage extends React.Component {
     )
   }
 }
-
-const CompanionInfo = () => (
-  <div className='mv4 tc navy f5' >
-    <Box style={{ background: 'rgba(105, 196, 205, 0.1)' }}>
-      <Trans i18nKey='companionInfo'>
-        <p className='ma0'>As you are using <strong>IPFS Companion</strong>, the files view is limited to files added while using the extension.</p>
-      </Trans>
-    </Box>
-  </div>
-)
-
-const AddFilesInfo = () => (
-  <div className='mv4 tc navy f5' >
-    <Box style={{ background: 'rgba(105, 196, 205, 0.1)' }}>
-      <Trans i18nKey='addFilesInfo'>
-        <p className='ma0'>Add files to your local IPFS node by clicking the <strong>Add to IPFS</strong> button above.</p>
-      </Trans>
-    </Box>
-  </div>
-)
-
-const WelcomeInfo = ({ t }) => (
-  <div className='flex'>
-    <div className='flex-auto pr3 lh-copy mid-gray'>
-      <Box>
-        <h1 className='mt0 mb3 montserrat fw4 f4 charcoal'>{t('welcomeInfo.header')}</h1>
-        <Trans i18nKey='welcomeInfo.paragraph1'>
-          <p className='f5'><a href='#/' className='link blue u b'>Check the status</a> of your node, it's Peer ID and connection info, the network traffic and the number of connected peers.</p>
-        </Trans>
-        <Trans i18nKey='welcomeInfo.paragraph2'>
-          <p className='f5'>Easily <a href='#/files' className='link blue b'>manage files</a> in your IPFS repo. You can drag and drop to add files, move and rename them, delete, share or download them.</p>
-        </Trans>
-        <Trans i18nKey='welcomeInfo.paragraph3'>
-          <p className='f5'>You can <a href='#/explore' className='link blue b'>explore IPLD data</a> that underpins how IPFS works.</p>
-        </Trans>
-        <Trans i18nKey='welcomeInfo.paragraph4'>
-          <p className='f5'>See all of your <a href='#/peers' className='link blue b'>connected peers</a>, geolocated by their IP address.</p>
-        </Trans>
-        <Trans i18nKey='welcomeInfo.paragraph5'>
-          <p className='mb4 f5'><a href='#/settings' className='link blue b'>Review the settings</a> for your IPFS node, and update them to better suit your needs.</p>
-        </Trans>
-        <Trans i18nKey='welcomeInfo.paragraph6'>
-          <p className='mb0 f5'>If you want to help push the Web UI forward, <a href='https://github.com/ipfs-shipyard/ipfs-webui' className='link blue'>check out its code</a> or <a href='https://github.com/ipfs-shipyard/ipfs-webui/issues' className='link blue'>report a bug</a>!</p>
-        </Trans>
-      </Box>
-    </div>
-    <div className='measure lh-copy dn db-l flex-none mid-gray f6' style={{ maxWidth: '40%' }}>
-      <AboutIpfs />
-    </div>
-  </div>
-)
 
 export default connect(
   'selectIpfsConnected',
---
+++ src/files/info/CompanionInfo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Trans } from 'react-i18next'
+import Box from '../../components/box/Box'
+
+export default () => (
+  <div className='mv4 tc navy f5' >
+    <Box style={{ background: 'rgba(105, 196, 205, 0.1)' }}>
+      <Trans i18nKey='companionInfo'>
+        <p className='ma0'>As you are using <strong>IPFS Companion</strong>, the files view is limited to files added while using the extension.</p>
+      </Trans>
+    </Box>
+  </div>
+)
