745f5fbebb9085ce63de27253bab2de3a9f4157a
Henrique Dias <hacdias@gmail.com>
2019-05-08 21:55:07 +0100

    wip
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/files/header/Header.js
+++ src/files/header/Header.js
@@ -18,34 +18,43 @@ class Header extends React.Component {
   render () {
     const {
       files, writeFilesProgress, t,
+      repoSize,
+      filesIsMfs,
+      repoNumObjects,
       doFilesNavigateTo
     } = this.props
 
     return (
-      <div className='flex flex-wrap items-center mb3'>
-        <Breadcrumbs path={files.path} onClick={doFilesNavigateTo} />
+      <div>
+        <div>
+          {repoNumObjects || 'N/A' } Blocks
+        </div>
 
-        { files.type === 'directory'
-          ? (
-            <div className='ml-auto flex items-center'>
-              <Button
-                className='mr3 f6 pointer'
-                color='charcoal-muted'
-                bg='bg-transparent'
-                onClick={this.props.onNewFolder}>
-                <FolderIcon viewBox='10 15 80 80' height='20px' className='fill-charcoal-muted w2 v-mid' />
-                <span className='fw3'>{t('newFolder')}</span>
-              </Button>
-              <FileInput
-                onAddFiles={this.props.onAdd}
-                onAddByPath={this.props.onAddByPath}
-                addProgress={writeFilesProgress} />
-            </div>
-          ) : (
-            <div ref={el => { this.dotsWrapper = el }} className='ml-auto' style={{ width: '1.5rem' }}> {/* to render correctly in Firefox */}
-              <GlyphDots className='fill-gray-muted pointer hover-fill-gray transition-all' onClick={this.handleContextMenu} />
-            </div>
-          )}
+        <div className='flex flex-wrap items-center mb3'>
+          <Breadcrumbs path={files.path} onClick={doFilesNavigateTo} />
+
+          { files.type === 'directory'
+            ? (
+              <div className='ml-auto flex items-center'>
+                <Button
+                  className='mr3 f6 pointer'
+                  color='charcoal-muted'
+                  bg='bg-transparent'
+                  onClick={this.props.onNewFolder}>
+                  <FolderIcon viewBox='10 15 80 80' height='20px' className='fill-charcoal-muted w2 v-mid' />
+                  <span className='fw3'>{t('newFolder')}</span>
+                </Button>
+                <FileInput
+                  onAddFiles={this.props.onAdd}
+                  onAddByPath={this.props.onAddByPath}
+                  addProgress={writeFilesProgress} />
+              </div>
+            ) : (
+              <div ref={el => { this.dotsWrapper = el }} className='ml-auto' style={{ width: '1.5rem' }}> {/* to render correctly in Firefox */}
+                <GlyphDots className='fill-gray-muted pointer hover-fill-gray transition-all' onClick={this.handleContextMenu} />
+              </div>
+            )}
+          </div>
       </div>
     )
   }
@@ -54,6 +63,9 @@ class Header extends React.Component {
 export default connect(
   'doFilesNavigateTo',
   'selectFiles',
+  'selectRepoSize',
+  'selectRepoNumObjects',
+  'selectFilesIsMfs',
   'selectWriteFilesProgress',
   translate('files')(Header)
 )
