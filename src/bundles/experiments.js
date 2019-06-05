f8d1a43a80e5dd74ae09e73e5c8151e06317507e
Chris Waring <chris@wwaves.co>
2019-06-05 23:13:16 +0100

    refactor actions/reducer + wire in desktop method

--- src/bundles/experiments.js
+++ src/bundles/experiments.js
@@ -1,12 +1,13 @@
-const ACTIONS = {
-  EXP_TOGGLE: 'EXPERIMENTS_EXP_TOGGLE'
+export const ACTIONS = {
+  EXP_TOGGLE_SUCCESS: 'EXPERIMENTS_TOGGLE_SUCCESS',
+  EXP_TOGGLE_FAIL: 'EXPERIMENTS_TOGGLE_FAIL',
+  EXP_UPDATE_STATE: 'EXPERIMENTS_UPDATE_STATE'
 }
 
 // setup experiments
-const EXPERIMENTS = {
-  npm: {
-    action: async () => null,
-    enabled: false,
+const getExperiments = store => ({
+  npmOnIpfs: {
+    action: async enabled => store.doDesktopToggleExperiment('npmOnIpfs'),
     actionUrls: [
       {
         url: 'https://github.com/ipfs-shipyard/npm-on-ipfs',
@@ -21,26 +22,16 @@ const EXPERIMENTS = {
         key: 'feedbackUrl'
       }
     ],
-    desktop: false
-  },
-  tpd: {
-    action: async () => null,
-    enabled: false
-  },
-  tpda: {
-    action: async () => null,
-    enabled: false
-  },
-  tpdb: {
-    action: async () => null,
-    enabled: false
+    desktop: true
   }
-}
+})
 
 // helpers
-const createAction = (type, key) => ({
-  type: type,
-  payload: { key }
+const getExperiment = (store, key) => getExperiments(store)[key]
+
+const createAction = (type, payload) => ({
+  type,
+  payload
 })
 
 const objAsArr = obj =>
@@ -70,27 +61,65 @@ export default {
   name: 'experiments',
   // persist all actions
   persistActions: Object.values(ACTIONS),
+
+  init: store => {
+    // get enabled desktop experiments and set as default state
+    if (window.ipfsDesktop) {
+      store.doExpSetEnabled(store.selectDesktopSettings().experiments || {})
+    }
+  },
+
   reducer: (state = {}, action) => {
-    if (action.type === ACTIONS.EXP_TOGGLE) {
+    if (action.type === ACTIONS.EXP_UPDATE_STATE) {
+      const newState = Object.keys(action.payload).reduce(
+        (all, key) => ({
+          ...all,
+          [key]: {
+            ...state[key],
+            ...action.payload[key]
+          }
+        }),
+        state
+      )
+      return newState
+    }
+    if (action.type === ACTIONS.EXP_TOGGLE_SUCCESS) {
       return toggleEnabled(state, action.payload.key)
     }
+
+    if (action.type === ACTIONS.EXP_TOGGLE_FAIL) {
+      // do something on fail
+    }
+
     return state
   },
-  doExpToggleAction: (key, enabled) => async ({ dispatch }) => {
+
+  doExpToggleAction: (key, enabled) => async ({ dispatch, store }) => {
     if (!key) return
 
-    const experiment = EXPERIMENTS[key]
-    const dispatchAction = createAction(ACTIONS.EXP_TOGGLE, key)
+    const experiment = getExperiment(store, key)
 
     if (typeof experiment.action === 'function') {
       await Promise.resolve(experiment.action(enabled))
-        .then(res => dispatch(dispatchAction))
-        .catch(err => console.error(err))
+        .then(res =>
+          dispatch(createAction(ACTIONS.EXP_TOGGLE_SUCCESS, { key }))
+        )
+        .catch(err => {
+          dispatch(createAction(ACTIONS.EXP_TOGGLE_FAIL, { key }))
+          console.error(err)
+        })
     } else {
-      dispatch(dispatchAction)
+      dispatch(createAction(ACTIONS.EXP_TOGGLE_SUCCESS, { key }))
     }
   },
-  selectExperiments: () =>
-    objAsArr(EXPERIMENTS).filter(e => !!e.desktop === !!window.ipfsDesktop),
+
+  doExpSetEnabled: state => ({ dispatch }) => {
+    dispatch(createAction(ACTIONS.EXP_UPDATE_STATE, state))
+  },
+
+  selectExperiments: state =>
+    objAsArr(getExperiments()).filter(
+      e => !!e.desktop === !!window.ipfsDesktop
+    ),
   selectExpState: state => state.experiments
 }
