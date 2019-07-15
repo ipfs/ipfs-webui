da9b58548438792fb80969408e321a9895f0d3fd
Henrique Dias <hacdias@gmail.com>
2019-07-15 11:28:00 +0100

    fix: use same font size and color on desktop settings
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/settings/DesktopSettings.js
+++ src/settings/DesktopSettings.js
@@ -19,9 +19,9 @@ const CheckboxSetting = ({ children, title, ...props }) => (
       <p className='ma0 f6 b'>{title}</p>
     </div>
 
-    <div className='pl4'>
+    <p className='pl4 charcoal mw7 mb0 mt1 f6 lh-copy'>
       <span>{children}</span>
-    </div>
+    </p>
   </div>
 )
 
@@ -35,38 +35,32 @@ export function DesktopSettings ({ t, doDesktopSettingsToggle, desktopSettings }
       <CheckboxSetting checked={desktopSettings['autoLaunch'] || false}
         title={t('launchOnStartup')}
         onChange={() => doDesktopSettingsToggle('autoLaunch')} />
-      <CheckboxSetting checked={desktopSettings['screenshotShortcut'] || false}
-        title={t('autoAddScreenshots')}
-        onChange={() => doDesktopSettingsToggle('screenshotShortcut')}>
-        <p className='mb0 mt1 lh-copy'>
-          <Trans
-            i18nKey='autoAddScreenshotsDescription'
-            defaults='Use <0>{ctrlKey}</0> + <2>{altKey}</2> + <3>S</3> to take screenshots and add them to the repository.'
-            values={{ ctrlKey, altKey }}
-            components={[<Key>0</Key>, <Key>0</Key>, <Key>0</Key>, <Key>0</Key>]} />
-        </p>
-      </CheckboxSetting>
-      <CheckboxSetting checked={desktopSettings['downloadHashShortcut'] || false}
-        title={t('downloadCopiedHash')}
-        onChange={() => doDesktopSettingsToggle('downloadHashShortcut')}>
-        <p className='mb0 mt1 lh-copy'>
-          <Trans
-            i18nKey='downloadCopiedHashDescription'
-            defaults='Use <0>{ctrlKey}</0> + <2>{altKey}</2> + <3>D</3> to download the last copied hash.'
-            values={{ ctrlKey, altKey }}
-            components={[<Key>0</Key>, <Key>0</Key>, <Key>0</Key>, <Key>0</Key>]} />
-        </p>
-      </CheckboxSetting>
       <CheckboxSetting checked={desktopSettings['ipfsOnPath'] || false}
         title={t('ipfsCmdTools')}
         disabled={os.platform() === 'win32'}
         onChange={() => doDesktopSettingsToggle('ipfsOnPath')}>
-        <p className='mb0 mt1 lh-copy'>
-          <Trans
-            i18nKey='ipfsCmdToolsDescription'
-            defaults='Add <0>ipfs</0> binary to your system <0>PATH</0> so you can use it in the command line.'
-            components={[<Key>0</Key>, <Key>0</Key>, <Key>0</Key>, <Key>0</Key>]} />
-        </p>
+        <Trans
+          i18nKey='ipfsCmdToolsDescription'
+          defaults='Add <0>ipfs</0> binary to your system <0>PATH</0> so you can use it in the command line.'
+          components={[<Key>0</Key>, <Key>0</Key>, <Key>0</Key>, <Key>0</Key>]} />
+      </CheckboxSetting>
+      <CheckboxSetting checked={desktopSettings['screenshotShortcut'] || false}
+        title={t('autoAddScreenshots')}
+        onChange={() => doDesktopSettingsToggle('screenshotShortcut')}>
+        <Trans
+          i18nKey='autoAddScreenshotsDescription'
+          defaults='Use <0>{ctrlKey}</0> + <2>{altKey}</2> + <3>S</3> to take screenshots and add them to the repository.'
+          values={{ ctrlKey, altKey }}
+          components={[<Key>0</Key>, <Key>0</Key>, <Key>0</Key>, <Key>0</Key>]} />
+      </CheckboxSetting>
+      <CheckboxSetting checked={desktopSettings['downloadHashShortcut'] || false}
+        title={t('downloadCopiedHash')}
+        onChange={() => doDesktopSettingsToggle('downloadHashShortcut')}>
+        <Trans
+          i18nKey='downloadCopiedHashDescription'
+          defaults='Use <0>{ctrlKey}</0> + <2>{altKey}</2> + <3>D</3> to download the last copied hash.'
+          values={{ ctrlKey, altKey }}
+          components={[<Key>0</Key>, <Key>0</Key>, <Key>0</Key>, <Key>0</Key>]} />
       </CheckboxSetting>
     </Box>
   )
