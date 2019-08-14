cd0a1ddfc4f80ccc94bbad8521ab83a81f7ef4c6
Henrique Dias <hacdias@gmail.com>
2019-08-14 14:49:17 +0200

    chore: update ipld (#1096)
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/bundles/index.js
+++ src/bundles/index.js
@@ -50,28 +50,7 @@ export default composeBundles(
   redirectsBundle,
   toursBundle,
   filesBundle(),
-  exploreBundle(async () => {
-    const ipldDeps = await Promise.all([
-      import(/* webpackChunkName: "ipld" */ 'ipld'),
-      import(/* webpackChunkName: "ipld" */ 'ipld-bitcoin'),
-      import(/* webpackChunkName: "ipld" */ 'ipld-dag-cbor'),
-      import(/* webpackChunkName: "ipld" */ 'ipld-dag-pb'),
-      import(/* webpackChunkName: "ipld" */ 'ipld-git'),
-      import(/* webpackChunkName: "ipld" */ 'ipld-raw'),
-      import(/* webpackChunkName: "ipld" */ 'ipld-zcash'),
-      import(/* webpackChunkName: "ipld" */ 'ipld-ethereum')
-    ])
-
-    // CommonJs exports object is .default when imported ESM style
-    const [ipld, ...formats] = ipldDeps.map(mod => mod.default)
-    // ipldEthereum is an Object, each key points to a ipld format impl
-    const ipldEthereum = formats.pop()
-    formats.push(...Object.values(ipldEthereum))
-    return {
-      ipld,
-      formats
-    }
-  }),
+  exploreBundle(),
   configBundle,
   configSaveBundle,
   nodeBandwidthBundle,
