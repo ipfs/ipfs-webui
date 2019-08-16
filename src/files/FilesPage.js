4e9ee1f216682cdeda034230d2b55af6449ae58f
Henrique Dias <hacdias@gmail.com>
2019-08-16 16:42:36 +0200

    fix: non unixfs on files (#1115)
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/files/FilesPage.js
+++ src/files/FilesPage.js
@@ -134,13 +134,27 @@ class FilesPage extends React.Component {
   }
 
   get mainView () {
-    const { files } = this.props
+    const { files, doExploreUserProvidedPath } = this.props
 
     if (!files) {
       return (<div></div>)
     }
 
-    if (files.type !== 'directory') {
+    if (files.type === 'unknown') {
+      const path = files.path.startsWith('/pins')
+        ? files.path.slice(6)
+        : files.path
+
+      return (
+        <div>
+          <Trans i18nKey='cidNotFileNorDir'>
+            The current link isn't a file, nor a directory. Try to <span className='link blue pointer' onClick={() => doExploreUserProvidedPath(path)}>inspect</span> it instead.
+          </Trans>
+        </div>
+      )
+    }
+
+    if (files.type === 'file') {
       return (
         <FilePreview {...files} />
       )
@@ -202,6 +216,7 @@ class FilesPage extends React.Component {
           handleClick={this.handleContextMenu}
           isUpperDir={contextMenu.file && contextMenu.file.name === '..'}
           isMfs={filesPathInfo ? filesPathInfo.isMfs : false}
+          isUnknown={contextMenu.file && contextMenu.file.type === 'unknown'}
           pinned={contextMenu.file && contextMenu.file.pinned}
           showDots={false}
           hash={contextMenu.file && contextMenu.file.hash}
@@ -295,5 +310,6 @@ export default connect(
   'selectToursEnabled',
   'doFilesWrite',
   'doFilesDownloadLink',
+  'doExploreUserProvidedPath',
   withTour(withTranslation('files')(FilesPage))
 )
