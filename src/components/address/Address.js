8083035264c080221cdbad050871fa7b8533308b
Henrique Dias <hacdias@gmail.com>
2019-09-15 11:31:01 +0100

    fix: correctly highlight p2p-circuit addresses
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/components/address/Address.js
+++ src/components/address/Address.js
@@ -3,11 +3,13 @@ import React from 'react'
 const Address = ({ value }) => {
   if (!value) return null
   const parts = value.split('/')
+  // used to shift the highlight one right on p2p-circuit addresses
+  const d = parts.includes('p2p-circuit') ? 1 : 0
   return (
     <div className='charcoal-muted monospace'>
       {parts.map((chunk, i) => (
         <span key={i}>
-          <span className={i % 2 || i > 4 ? 'force-select' : 'force-select charcoal'}>{chunk}</span>
+          <span className={(i - d) % 2 || (i - d) > 4 || (i - d) === 0 ? 'force-select' : 'force-select charcoal'}>{chunk}</span>
           {i < parts.length - 1 ? '/' : ''}
         </span>
       ))}
