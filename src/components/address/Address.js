d4af67efda91df9eeee6fa6d321bead6fc474431
Henrique Dias <hacdias@gmail.com>
2019-09-20 08:41:55 +0100

    feat: highlight everything but protocols (#1188)
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/components/address/Address.js
+++ src/components/address/Address.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import Multiaddr from 'multiaddr'
 
 const Address = ({ value }) => {
   if (!value) return null
+
+  const ma = Multiaddr(value)
+  const protos = ma.protoNames().concat(['ipfs', 'p2p'])
   const parts = value.split('/')
-  // used to shift the highlight one right on p2p-circuit addresses
-  const d = parts.includes('p2p-circuit') ? 1 : 0
+
   return (
     <div className='charcoal-muted monospace'>
       {parts.map((chunk, i) => (
         <span key={i}>
-          <span className={(i - d) % 2 || (i - d) > 4 || (i - d) === 0 ? 'force-select' : 'force-select charcoal'}>{chunk}</span>
+          <span className={protos.includes(chunk) ? 'force-select' : 'force-select charcoal'}>{chunk}</span>
           {i < parts.length - 1 ? '/' : ''}
         </span>
       ))}
