2be20388360f623e890e444e0d58c73db74ee132
Henrique Dias <hacdias@gmail.com>
2019-10-20 20:04:51 +0100

    feat: improve peers locations performance
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/bundles/peer-locations.js
+++ src/bundles/peer-locations.js
@@ -1,6 +1,7 @@
-import { createSelector } from 'redux-bundler'
+import { createAsyncResourceBundle, createSelector } from 'redux-bundler'
 import { getConfiguredCache } from 'money-clip'
 import geoip from 'ipfs-geoip'
+import queue from 'queue'
 import Multiaddr from 'multiaddr'
 import ms from 'milliseconds'
 
@@ -12,243 +13,90 @@ export default function (opts) {
   // Cache options
   opts.cache = opts.cache || {}
 
-  const defaultState = {
-    // Peer locations keyed by peer ID then peer address.
-    // i.e. { [peerId]: { [multiaddr]: { state, data?, error? } } }
-    // `state` can be queued, resolving, resolved or failed
-    // `data` is the resolved location data (see ipfs-geoip docs)
-    // `error` is only present if state is 'failed'
-    locations: {},
-    // Peer IDs in the queue for resolving ONE of their queued addresses.
-    // We actually have a queue of queues since each peer may have multiple
-    // addresses to resolve. Peer locations are resolved in PARALLEL but for
-    // any given peer we work our way through it's list of addresses in SERIES
-    // until we find one that resolves.
-    queuingPeers: [],
-    // Peer IDs currently resolving for one of their queued addresses
-    resolvingPeers: []
-  }
+  let peerLocResolver = new PeerLocationResolver(opts)
 
-  const peerLocResolver = new PeerLocationResolver(opts)
-
-  return {
+  const bundle = createAsyncResourceBundle({
     name: 'peerLocations',
+    actionBaseType: 'PEER_LOCATIONS',
+    getPromise: async ({ store, getIpfs }) => {
+      const peers = store.selectPeers()
+      const locations = store.selectPeerLocations() || {}
 
-    reducer (state = defaultState, action) {
-      if (action.type === 'PEER_LOCATIONS_PEERS_QUEUED') {
-        const { addrsByPeer, peerByAddr } = action.payload
+      for (const p of peers) {
+        const peerId = p.peer.toB58String()
+        const addr = p.addr.toString()
 
-        return {
-          ...state,
-          queuingPeers: state.queuingPeers.concat(Object.keys(addrsByPeer)),
-          locations: Object.keys(peerByAddr).reduce((locs, addr) => {
-            const peerId = peerByAddr[addr]
-            locs[peerId] = locs[peerId] || {}
-            locs[peerId][addr] = { state: 'queued' }
-            return locs
-          }, { ...state.locations })
-        }
-      }
-
-      if (action.type === 'PEER_LOCATIONS_RESOLVE_STARTED') {
-        const { peerId, addr } = action.payload
-
-        return {
-          ...state,
-          queuingPeers: state.queuingPeers.filter(id => id !== peerId),
-          resolvingPeers: state.resolvingPeers.concat(peerId),
-          locations: {
-            ...state.locations,
-            [peerId]: {
-              ...state.locations[peerId],
-              [addr]: { state: 'resolving' }
-            }
+        if (!locations[peerId]) {
+          const res = peerLocResolver.checkPeer(peerId, addr, getIpfs)
+          if (res) {
+            locations[peerId] = res
           }
         }
       }
 
-      if (action.type === 'PEER_LOCATIONS_RESOLVE_FINISHED') {
-        const { peerId, addr, location } = action.payload
-
-        return {
-          ...state,
-          resolvingPeers: state.resolvingPeers.filter(id => id !== peerId),
-          locations: {
-            ...state.locations,
-            [peerId]: {
-              ...state.locations[peerId],
-              [addr]: {
-                state: 'resolved',
-                data: location
-              }
-            }
-          }
-        }
-      }
-
-      if (action.type === 'PEER_LOCATIONS_RESOLVE_FAILED') {
-        const { peerId, addr } = action.payload
-
-        // Is there another queued address for this peer?
-        const hasAlternate = Object.keys(state.locations[peerId])
-          .filter(a => a !== addr)
-          .some(a => state.locations[peerId][a].state === 'queued')
-
-        return {
-          ...state,
-          resolvingPeers: state.resolvingPeers.filter(id => id !== peerId),
-          // Re-queue the peer if it has another address to try
-          queuingPeers: hasAlternate
-            ? state.queuingPeers.concat(peerId)
-            : state.queuingPeers,
-          locations: {
-            ...state.locations,
-            [peerId]: {
-              ...state.locations[peerId],
-              [addr]: {
-                state: 'failed',
-                error: action.payload.error
-              }
-            }
-          }
-        }
-      }
-
-      return state
+      return locations
     },
+    staleAfter: ms.seconds(3),
+    retryAfter: ms.seconds(3),
+    persist: false,
+    checkIfOnline: false
+  })
 
-    // Returns an object of the form:
-    // { [peerId]: { [multiaddr]: { state, data?, error? } } }
-    selectPeerLocationsRaw: state => state.peerLocations.locations,
-
-    // Select just the data for the peer locations that have been resolved
-    // Returns an object of the form:
-    // { [peerId]: { /* location data */ } }
-    selectPeerLocations: createSelector(
-      'selectPeerLocationsRaw',
-      peerLocsRaw => Object.keys(peerLocsRaw).reduce((locs, peerId) => {
-        const locsByAddr = peerLocsRaw[peerId]
-        const addr = Object.keys(locsByAddr).find(a => locsByAddr[a].state === 'resolved')
-        if (addr) locs[peerId] = peerLocsRaw[peerId][addr].data
-        return locs
-      }, {})
-    ),
-
-    selectPeerLocationsQueuingPeers: state => state.peerLocations.queuingPeers,
-
-    selectPeerLocationsResolvingPeers: state => state.peerLocations.resolvingPeers,
-
-    selectPeerLocationsForSwarm: createSelector(
-      'selectPeers',
-      'selectPeerLocations',
-      'selectBootstrapPeers',
-      (peers, locations, bootstrapPeers) => peers && peers.map(peer => {
-        const peerId = peer.peer.toB58String()
-        const locationObj = locations[peerId]
-        const location = toLocationString(locationObj)
-        const flagCode = locationObj && locationObj.country_code
-        const coordinates = locationObj && [
-          locationObj.longitude,
-          locationObj.latitude
-        ]
-        const connection = parseConnection(peer.addr)
-        const address = peer.addr.toString()
-        const latency = parseLatency(peer.latency)
-        const notes = parseNotes(peer, bootstrapPeers)
-
-        return {
-          peerId,
-          location,
-          flagCode,
-          coordinates,
-          connection,
-          address,
-          latency,
-          notes
-        }
-      })
-    ),
-
-    selectPeerCoordinates: createSelector(
-      'selectPeerLocationsForSwarm',
-      peers => {
-        if (!peers) return []
-        const allCoord = peers
-          .map(p => p.coordinates)
-          .filter(arr => !!arr)
-
-        const unique = new Set(allCoord.map(JSON.stringify))
-        return Array.from(unique).map(JSON.parse)
+  bundle.reactPeerLocationsFetch = createSelector(
+    'selectPeerLocationsShouldUpdate',
+    'selectIpfsReady',
+    (shouldUpdate, ipfsReady) => {
+      if (shouldUpdate && ipfsReady) {
+        return { actionCreator: 'doFetchPeerLocations' }
       }
-    ),
+    }
+  )
 
-    doResolvePeerLocation: ({ peerId, addr }) => async ({ dispatch, store, getIpfs }) => {
-      dispatch({ type: 'PEER_LOCATIONS_RESOLVE_STARTED', payload: { peerId, addr } })
+  bundle.selectPeerLocationsForSwarm = createSelector(
+    'selectPeers',
+    'selectPeerLocations',
+    'selectBootstrapPeers',
+    (peers, locations = {}, bootstrapPeers) => peers && peers.map(peer => {
+      const peerId = peer.peer.toB58String()
+      const locationObj = locations ? locations[peerId] : null
+      const location = toLocationString(locationObj)
+      const flagCode = locationObj && locationObj.country_code
+      const coordinates = locationObj && [
+        locationObj.longitude,
+        locationObj.latitude
+      ]
+      const connection = parseConnection(peer.addr)
+      const address = peer.addr.toString()
+      const latency = parseLatency(peer.latency)
+      const notes = parseNotes(peer, bootstrapPeers)
 
-      let location = null
-      try {
-        location = await peerLocResolver.lookupWithCaches(peerId, addr, getIpfs)
-      } catch (err) {
-        return dispatch({
-          type: 'PEER_LOCATIONS_RESOLVE_FAILED',
-          payload: { peerId, addr, error: err }
-        })
+      return {
+        peerId,
+        location,
+        flagCode,
+        coordinates,
+        connection,
+        address,
+        latency,
+        notes
       }
+    })
+  )
 
-      dispatch({
-        type: 'PEER_LOCATIONS_RESOLVE_FINISHED',
-        payload: { peerId, addr, location }
-      })
-    },
+  bundle.selectPeerCoordinates = createSelector(
+    'selectPeerLocationsForSwarm',
+    peers => {
+      if (!peers) return []
+      const allCoord = peers
+        .map(p => p.coordinates)
+        .filter(arr => !!arr)
 
-    // Resolve another peer location where there's a peer in the queue and we're
-    // not already resolving more than our allowed concurrency
-    reactResolvePeerLocation: createSelector(
-      'selectHash',
-      'selectIpfsConnected',
-      'selectPeerLocationsRaw',
-      'selectPeerLocationsQueuingPeers',
-      'selectPeerLocationsResolvingPeers',
-      (hash, ipfsConnected, peerLocationsRaw, queuingPeers, resolvingPeers) => {
-        if (hash === '/peers' && ipfsConnected && queuingPeers.length && resolvingPeers.length < opts.concurrency) {
-          const peerId = queuingPeers[0]
-          const locsByAddr = peerLocationsRaw[peerId]
+      const unique = new Set(allCoord.map(JSON.stringify))
+      return Array.from(unique).map(JSON.parse)
+    }
+  )
 
-          // TODO: what is causing this to fail?
-          if (!locsByAddr) {
-            return
-          }
-
-          const addr = Object.keys(locsByAddr).find(a => locsByAddr[a].state === 'queued')
-          return { actionCreator: 'doResolvePeerLocation', args: [{ peerId, addr }] }
-        }
-      }
-    ),
-
-    // When the peers list changes, queue up the peers we don't already know about
-    reactQueuePeerLocations: createSelector(
-      'selectPeers',
-      'selectPeerLocationsRaw',
-      (peers, peerLocationsRaw) => {
-        const payload = (peers || []).reduce(({ addrsByPeer, peerByAddr }, p) => {
-          const peerId = p.peer.toB58String()
-          const addr = p.addr.toString()
-
-          if (peerLocationsRaw[peerId] && peerLocationsRaw[peerId][addr]) {
-            return { addrsByPeer, peerByAddr }
-          }
-
-          addrsByPeer[peerId] = (addrsByPeer[peerId] || []).concat(addr)
-          peerByAddr[addr] = peerId
-          return { addrsByPeer, peerByAddr }
-        }, { addrsByPeer: {}, peerByAddr: {} })
-
-        if (Object.keys(payload.addrsByPeer).length) {
-          return { type: 'PEER_LOCATIONS_PEERS_QUEUED', payload }
-        }
-      }
-    )
-  }
+  return bundle
 }
 
 const isNonHomeIPv4 = t => t[0] === 4 && t[1] !== '127.0.0.1'
@@ -300,44 +148,66 @@ class PeerLocationResolver {
       ...opts.cache
     })
 
+    this.peersToAddrs = {}
+    this.failedAddrs = []
+
+    this.queue = queue({
+      concurrency: opts.concurrency,
+      autostart: true
+    })
+
     this.geoipLookupPromises = {}
   }
 
-  async lookupWithCaches (peerId, addr, getIpfs) {
+  checkPeer (peerId, addr, getIpfs) {
+    if (this.peersToAddrs[peerId]) {
+      return this.peersToAddrs[peerId]
+    }
+
     const ipv4Tuple = Multiaddr(addr).stringTuples().find(isNonHomeIPv4)
     if (!ipv4Tuple) {
-      throw new Error(`Unable to resolve location for non-IPv4 address ${addr}`)
+      return
+      // throw new Error(`Unable to resolve location for non-IPv4 address ${addr}`)
     }
 
     var ipv4Addr = ipv4Tuple[1]
 
-    // maybe we have it cached by ipv4 address already, check that.
-    let location = await this.geoipCache.get(ipv4Addr)
-    if (location) {
-      return location
+    // ignore lookups that previously failed
+    if (this.failedAddrs.includes(ipv4Addr)) {
+      return
     }
 
     // no ip address cached. are we looking it up already?
     var locPromise = this.geoipLookupPromises[ipv4Addr]
 
     if (!locPromise) {
-      locPromise = new Promise((resolve, reject) => {
-        const ipfs = getIpfs()
-        geoip.lookup(ipfs, ipv4Addr, (err, data) => {
-          // save it in the caches, and remove the promise.
-          this.geoipCache.set(ipv4Addr, data)
-          delete this.geoipLookupPromises[ipv4Addr]
+      this.queue.push(async () => {
 
-          if (err) return reject(err)
-          resolve(data)
+        // maybe we have it cached by ipv4 address already, check that.
+        let location = await this.geoipCache.get(ipv4Addr)
+        if (location) {
+          return location
+        }
+
+        return new Promise((resolve, reject) => {
+          const ipfs = getIpfs()
+
+          geoip.lookup(ipfs, ipv4Addr, (err, data) => {
+            delete this.geoipLookupPromises[ipv4Addr]
+
+            if (err) {
+              // mark this one as failed so we don't retry again
+              this.failedAddrs.push(ipv4Addr)
+              return resolve()
+            }
+
+            // save the data!
+            this.geoipCache.set(ipv4Addr, data)
+            this.peersToAddrs[peerId] = data
+            resolve()
+          })
         })
       })
-
-      // set the promise here, so we only look it up once.
-      this.geoipLookupPromises[ipv4Addr] = locPromise
     }
-
-    location = await locPromise
-    return location
   }
 }
