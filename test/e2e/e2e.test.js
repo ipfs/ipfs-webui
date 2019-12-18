7a6d7193fd0eac329e1bfaa5219454874ae74eed
Marcin Rataj <lidel@lidel.org>
2019-12-18 13:37:32 +0100

    test: e2e for Explore screen

--- test/e2e/e2e.test.js
+++ test/e2e/e2e.test.js
@@ -104,11 +104,41 @@ describe('Files screen', () => {
     await page.waitForSelector('.File')
     await expect(page).toMatch(filename)
 
-    // add file manually to get expected CID
+    // add file manually to get expected CID (with default params)
     const expectedData = fs.readFileSync(filename, 'utf8')
     const [result] = await ipfs.add(expectedData)
 
     // expect CID to be present on the page
     await expect(page).toMatch(result.hash)
   })
+})
+
+
+describe('Explore screen', () => {
+  beforeAll(async () => {
+    await page.goto(landingPage + '#/explore')
+  })
+
+  it('should have Project Apollo Archive as one of examples', async () => {
+    await page.waitForSelector('a[href="#/explore/QmSnuWmxptJZdLJpKRarxBMS2Ju2oANVrgbr2xWbie9b2D"]')
+    await expect(page).toMatch('Project Apollo Archive')
+    await expect(page).toMatch('QmSnuWmxptJZdLJpKRarxBMS2Ju2oANVrgbr2xWbie9b2D')
+
+  })
+
+  it('should open arbitrary CID', async () => {
+    // add a local file to repo so test is fast and works in offline mode
+    const cid = 'bafkreicgkmwhdunxgdqwqveecdo3wqmgulb4azm6sfnrtvd7g47mnrixji'
+    const expectedData = fs.readFileSync('LICENSE', 'utf8')
+    const [result] = await ipfs.add(expectedData, { cidVersion: 1 })
+    await expect(result.hash).toStrictEqual(cid)
+
+    // open inspector
+    await page.goto(landingPage + `#/explore/${cid}`)
+    // expect node type
+    await expect(page).toMatch('DAG Node')
+    // expect cid details
+    await expect(page).toMatch('base32 - cidv1 - raw - sha2-256-256-46532c71d1b730e168548410ddbb4186a2c3c0659e915b19d47f373ec6c5174a')
+  })
+
 })
