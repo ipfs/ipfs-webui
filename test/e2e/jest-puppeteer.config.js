56c95dc405c331ab3a515ca257b8c1191236917f
Marcin Rataj <lidel@lidel.org>
2020-01-10 16:29:43 +0100

    chore: support DEBUG output on travis

--- test/e2e/jest-puppeteer.config.js
+++ test/e2e/jest-puppeteer.config.js
@@ -1,8 +1,9 @@
 const debug = process.env.DEBUG === 'true'
+const travis = process.env.TRAVIS === 'true'
 module.exports = {
   launch: {
     dumpio: debug, // print all IO to the console
-    headless: !debug, // show browser window when in debug mode
+    headless: (!debug || travis), // show browser window when in debug mode
     slowMo: debug ? 50 : undefined // slow down scripted tests when debugging in non-headless mode
   }
 }
