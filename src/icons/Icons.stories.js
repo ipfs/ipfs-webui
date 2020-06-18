bdd62309140addfbf2f2e9f4629b6c4409a4590a
Rafael Ramalho <rafazelramalho19@gmail.com>
2020-06-18 18:31:06 +0100

    chore: add icons stories

---
+++ src/icons/Icons.stories.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withKnobs, text, number, color } from '@storybook/addon-knobs'
+
+const requireContext = require.context('.', true, /\.js?$/)
+const modules = requireContext.keys().filter((c) => !c.includes('.stories'))
+const icons = modules.map(m => ({
+  name: m.replace('./', '').replace('.js', ''),
+  Icon: requireContext(m).default
+}))
+
+const filterByTextQuery = (icon) => {
+  const searchQuery = text('Search', '')
+  return icon.name.includes(searchQuery)
+}
+
+storiesOf('Icons', module)
+  .addDecorator(withKnobs)
+  .add('List', () => {
+    const size = number('Size', 32, { range: true, min: 1, max: 200, step: 1 })
+    const fill = color('Fill', undefined)
+    const stroke = color('Stroke', undefined)
+
+    return (
+      <div className="flex w-100 flex-wrap">
+        {icons.filter(filterByTextQuery).map(({ Icon, name }) => (
+          <div className="flex items-center flex-column ma3">
+            <Icon fill={fill} stroke={stroke} width={size} height={size} className="transition-all" />
+            <span>{ name }</span>
+          </div>)
+        )}
+      </div>
+    )
+  })
