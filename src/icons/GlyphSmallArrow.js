0656fc92338fdd0d1b1eafc9d75b53b3137273a5
Rafael Ramalho <rafazelramalho19@gmail.com>
2020-07-16 14:56:51 +0100

    fix: small arrow fill (#1532)

--- src/icons/GlyphSmallArrow.js
+++ src/icons/GlyphSmallArrow.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const GlyphSmallArrows = props => (
   <svg viewBox="0 0 32 21" {...props}>
-    <path d="M16 20.2L0.5 4.7L4.4 0.800003L16 12.4L27.6 0.800003L31.5 4.7C26.3 9.8 21.2 15 16 20.2Z" fill="currentColor"/>
+    <path d="M16 20.2L0.5 4.7L4.4 0.800003L16 12.4L27.6 0.800003L31.5 4.7C26.3 9.8 21.2 15 16 20.2Z" />
   </svg>
 
 )
