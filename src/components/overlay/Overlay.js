aef81fb1987b9497ea02e787e57d79a03a74c202
Rafael Ramalho <rafazelramalho19@gmail.com>
2020-07-17 17:00:12 +0100

    Feat/pinning settings custom modal (#1546)
    Co-authored-by: Jessica Schilling <jessica@protocol.ai>

--- src/components/overlay/Overlay.js
+++ src/components/overlay/Overlay.js
@@ -2,18 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Modal } from 'react-overlays'
 
-function Overlay ({ children, show, onLeave, className, ...props }) {
-  const stopPropagation = (e) => {
+function Overlay ({ children, show, onLeave, className, hidden, ...props }) {
+  const handleKeyDown = (e) => {
+    if (e.key !== 'Escape') return
+
     e.stopPropagation()
     e.nativeEvent.stopImmediatePropagation()
 
-    if (e.key === 'Escape') {
-      onLeave()
-    }
+    onLeave()
   }
 
   const renderBackdrop = (props) => (
-    <div className='fixed top-0 left-0 right-0 bottom-0 bg-black o-50' {...props} />
+    <div className='fixed top-0 left-0 right-0 bottom-0 bg-black o-50' hidden={hidden} {...props} />
   )
 
   return (
@@ -22,7 +22,7 @@ function Overlay ({ children, show, onLeave, className, ...props }) {
       show={show}
       className={`${className} fixed top-0 left-0 right-0 bottom-0 z-max flex items-center justify-around`}
       renderBackdrop={renderBackdrop}
-      onKeyDown={stopPropagation}
+      onKeyDown={handleKeyDown}
       onBackdropClick={onLeave}>
       {children}
     </Modal>
