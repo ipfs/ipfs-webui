41450fb5f95171d0f79dfd416dfda3ad51a747c3
Irakli Gozalishvili <contact@gozala.io>
2020-07-29 09:54:59 -0700

    Update src/bundles/ipfs-provider.js
    Co-authored-by: Marcin Rataj <lidel@lidel.org>

--- src/bundles/ipfs-provider.js
+++ src/bundles/ipfs-provider.js
@@ -161,7 +161,7 @@ const writeSetting = (id, value) => {
   try {
     window.localStorage.setItem(id, JSON.stringify(value))
   } catch (error) {
-    console.log(`Error writing '${id}' value to localStorage`, error)
+    console.error(`Error writing '${id}' value to localStorage`, error)
   }
 }
 
