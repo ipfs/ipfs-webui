ed008eb7ed873024dcc2b9cea278e0ff51951bcf
Jessica Schilling <jessica@protocol.ai>
2020-07-31 14:25:36 -0600

    Responsive display tweak

--- src/welcome/WelcomePage.js
+++ src/welcome/WelcomePage.js
@@ -78,7 +78,7 @@ const ConnectionStatus = ({ t, connected, sameOrigin, ipfsApiAddress, doUpdateIp
           <GlyphTick style={{ height: 76 }} className='fill-green' role='presentation' />
           <h1 className='montserrat fw4 charcoal ma0 f3 green'>{t('connected.header')}</h1>
         </div>
-        <p className='fw6 mt1 ml3'>{t('connected.paragraph1')}</p>
+        <p className='fw6 mt1 ml3-ns w-100'>{t('connected.paragraph1')}</p>
       </div>
     )
   }
