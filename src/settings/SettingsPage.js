349065a39f49ddf20946f18b8ef7c7c522fb8109
Jessica Schilling <jessica@protocol.ai>
2020-08-14 12:49:05 -0600

    Remove non-functional anchor

--- src/settings/SettingsPage.js
+++ src/settings/SettingsPage.js
@@ -45,7 +45,7 @@ export const SettingsPage = ({
     </Box>
 
     <Box className='mb3 pa4 joyride-settings-customapi'>
-      <div className='lh-copy charcoal' id="api">
+      <div className='lh-copy charcoal'>
         <Title>{t('api')}</Title>
         <Trans i18nKey='apiDescription' t={t}>
           <p>If your node is configured with a <a className='link blue' href='https://github.com/ipfs/go-ipfs/blob/master/docs/config.md#addresses' target='_blank' rel='noopener noreferrer'>custom API address</a>, including a port other than the default 5001, enter it here to update your config file.</p>
