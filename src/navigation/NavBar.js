11d5c0ae6ff817a9e87a0e3aa8e12a66ef0dd431
Jessica Schilling <jessica@protocol.ai>
2020-09-30 12:21:28 -0600

    Changes from @bertrandfalguiere in https://github.com/ipfs-shipyard/ipfs-webui/pull/1651

--- src/navigation/NavBar.js
+++ src/navigation/NavBar.js
@@ -55,6 +55,8 @@ export const NavBar = ({ t }) => {
   const bugsUrl = `${codeUrl}/issues`
   const gitRevision = process.env.REACT_APP_GIT_REV
   const revisionUrl = `${codeUrl}/commit/${gitRevision}`
+  const webUiVersion = process.env.REACT_APP_VERSION
+  const webUiVersionUrl = `${codeUrl}/releases/tag/v${webUiVersion}`
   return (
     <div className='h-100 fixed-l flex flex-column justify-between' style={{ overflowY: 'auto', width: 'inherit' }}>
       <div className='flex flex-column'>
@@ -73,6 +75,9 @@ export const NavBar = ({ t }) => {
         </div>
       </div>
       <div className='dn db-l navbar-footer mb3 tc center f7 o-80 glow'>
+        { webUiVersion && <div className='mb1'>
+          <a className='link white' href={webUiVersionUrl} target='_blank' rel='noopener noreferrer'>{t('app:terms.version')} {webUiVersion}</a>
+        </div> }
         { gitRevision && <div className='mb1'>
           <a className='link white' href={revisionUrl} target='_blank' rel='noopener noreferrer'>{t('app:nav.revision')} {gitRevision}</a>
         </div> }
