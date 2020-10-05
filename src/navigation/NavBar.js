8acb1353183cdc16d24ed63ddcde10d3d22e86d2
Jessica Schilling <jessica@protocol.ai>
2020-10-05 10:36:31 -0600

    feat: add version info in nav and Status screen, tighten nav accordingly (#1654)
    * Tightens nav; shows nav footer at 640+ high
    
    * Changes from @bertrandfalguiere in https://github.com/ipfs-shipyard/ipfs-webui/pull/1651
    
    * Tweak to remove scrollbar close to 640px
    
    * version -> UI

--- src/navigation/NavBar.js
+++ src/navigation/NavBar.js
@@ -29,7 +29,7 @@ const NavLink = ({
   const anchorClass = classnames({
     'bg-white-10 navbar-item-active': active,
     'o-50 no-pointer-events': disabled
-  }, ['dib db-l pt2 pb3 pv3-l white no-underline f5 hover-bg-white-10 tc bb bw2 bw0-l b--navy'])
+  }, ['dib db-l pt2 pb3 pv2-l white no-underline f5 hover-bg-white-10 tc bb bw2 bw0-l b--navy'])
   const svgClass = classnames({
     'o-100': active,
     'o-50': !active
@@ -40,7 +40,7 @@ const NavLink = ({
     <a href={disabled ? null : href} className={anchorClass} role='menuitem' title={children}>
       <div className='db ph2 pv1'>
         <div className='db'>
-          <Svg width='50' role='presentation' className={svgClass} />
+          <Svg width='46' role='presentation' className={svgClass} />
         </div>
         <div className={`${active ? 'o-100' : 'o-50'} db f6 tc montserrat ttu fw1 `} style={{ whiteSpace: 'pre-wrap' }}>
           {children}
@@ -55,12 +55,14 @@ export const NavBar = ({ t }) => {
   const bugsUrl = `${codeUrl}/issues`
   const gitRevision = process.env.REACT_APP_GIT_REV
   const revisionUrl = `${codeUrl}/commit/${gitRevision}`
+  const webUiVersion = process.env.REACT_APP_VERSION
+  const webUiVersionUrl = `${codeUrl}/releases/tag/v${webUiVersion}`
   return (
     <div className='h-100 fixed-l flex flex-column justify-between' style={{ overflowY: 'auto', width: 'inherit' }}>
       <div className='flex flex-column'>
         <a href="#/welcome" role='menuitem' title={t('welcome:description')}>
-          <div className='pt3 pb1 pv4-l'>
-            <img className='center db-l dn' style={{ height: 100 }} src={ipfsLogoTextVert} alt='' />
+          <div className='pt3 pb1 pv2-l'>
+            <img className='center db-l dn pv1' style={{ height: 94 }} src={ipfsLogoTextVert} alt='' />
             <img className='center db dn-l' style={{ height: 70 }} src={ipfsLogoTextHoriz} alt='' />
           </div>
         </a>
@@ -72,7 +74,10 @@ export const NavBar = ({ t }) => {
           <NavLink to='/settings' icon={StrokeSettings}>{t('settings:title')}</NavLink>
         </div>
       </div>
-      <div className='dn db-l navbar-footer mb3 tc center f7 o-80 glow'>
+      <div className='dn db-l navbar-footer mb2 tc center f7 o-80 glow'>
+        { webUiVersion && <div className='mb1'>
+          <a className='link white' href={webUiVersionUrl} target='_blank' rel='noopener noreferrer'>{t('app:terms.ui')} v{webUiVersion}</a>
+        </div> }
         { gitRevision && <div className='mb1'>
           <a className='link white' href={revisionUrl} target='_blank' rel='noopener noreferrer'>{t('app:nav.revision')} {gitRevision}</a>
         </div> }
