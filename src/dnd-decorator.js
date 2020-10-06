11dead31d9f29fc61985512ae939262b8ee4b9d6
Irakli Gozalishvili <contact@gozala.io>
2020-10-06 09:18:56 -0700

    fix: broken file list stories (#1657)

--- src/dnd-decorator.js
+++ src/dnd-decorator.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import DnDBackend from './lib/dnd-backend'
-import { DragDropContextProvider } from 'react-dnd'
+import { DndProvider } from 'react-dnd'
 
 export default function DndDecorator (fn) {
   return (
-    <DragDropContextProvider backend={DnDBackend}>
+    <DndProvider backend={DnDBackend}>
       {fn()}
-    </DragDropContextProvider>
+    </DndProvider>
   )
 }
