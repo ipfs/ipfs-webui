2746ae8f2e5badcb4729d8bf404584203281da23
Jack Michaud <mrmichaud42@gmail.com>
2020-10-19 08:55:21 -0700

    Change ApiAddressForm to more closely follow SelectPeer - Shows fail/success border without being selected - Does not refocus if it fails to connect

--- src/components/api-address-form/ApiAddressForm.js
+++ src/components/api-address-form/ApiAddressForm.js
@@ -4,16 +4,21 @@ import { withTranslation } from 'react-i18next'
 import Button from '../button/Button'
 import { checkValidAPIAddress } from '../../bundles/ipfs-provider';
 
-const ApiAddressForm = ({ t, doUpdateIpfsApiAddress, ipfsApiAddress }) => {
+const ApiAddressForm = ({ t, doUpdateIpfsApiAddress, ipfsApiAddress, ipfsInitFailed }) => {
   const [value, setValue] = useState(asAPIString(ipfsApiAddress))
-  const [showFailState, setShowFailState] = useState(!checkValidAPIAddress(value))
-  const inputElement = useRef()
-
+  const [showFailState, setShowFailState] = useState(!checkValidAPIAddress(value) || ipfsInitFailed)
+  //
   // Updates the border of the input as the user types.
   // If the provided multiaddr is valid, the border is green. Otherwise, it is red.
   useEffect(() => {
-    setShowFailState(!checkValidAPIAddress(value))
-  }, [value])
+    if (!checkValidAPIAddress(value)) {
+      setShowFailState(true)
+    } else if (ipfsInitFailed) {
+      setShowFailState(true)
+    } else {
+      setShowFailState(false)
+    }
+  }, [value, ipfsInitFailed])
 
   const onChange = (event) => setValue(event.target.value)
 
@@ -24,7 +29,6 @@ const ApiAddressForm = ({ t, doUpdateIpfsApiAddress, ipfsApiAddress }) => {
     // If the IPFS API address failed to update, refocus to the API address input
     if (!succeeded) {
       setShowFailState(true)
-      inputElement.current.focus()
     }
   }
 
@@ -40,11 +44,10 @@ const ApiAddressForm = ({ t, doUpdateIpfsApiAddress, ipfsApiAddress }) => {
         id='api-address'
         aria-label={t('apiAddressForm.apiLabel')}
         type='text'
-        className={`w-100 lh-copy monospace f5 pl1 pv1 mb2 charcoal input-reset ba b--black-20 br1 ${showFailState ? 'focus-outline-red' : 'focus-outline-green'}`}
+        className={`w-100 lh-copy monospace f5 pl1 pv1 mb2 charcoal input-reset ba b--black-20 br1 ${showFailState ? 'focus-outline-red b--red-muted' : 'focus-outline-green b--green-muted'}`}
         onChange={onChange}
         onKeyPress={onKeyPress}
         value={value}
-        ref={inputElement}
       />
       <div className='tr'>
         <Button className='tc'>{t('actions.submit')}</Button>
@@ -65,5 +68,6 @@ const asAPIString = (value) => {
 export default connect(
   'doUpdateIpfsApiAddress',
   'selectIpfsApiAddress',
+  'selectIpfsInitFailed',
   withTranslation('app')(ApiAddressForm)
 )
