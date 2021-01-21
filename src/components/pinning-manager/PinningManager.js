7b4b9b7219ebccea0626b0a492ab2eb0ba488185
Rafael Ramalho <rafazelramalho19@gmail.com>
2021-01-21 17:30:00 +0000

    feat: add remote pinning services to the settings page

--- src/components/pinning-manager/PinningManager.js
+++ src/components/pinning-manager/PinningManager.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect, useState, useMemo, useRef } from 'react'
 import { connect } from 'redux-bundler-react'
-import filesize from 'filesize'
+// import filesize from 'filesize'
 import { AutoSizer, Table, Column, SortDirection } from 'react-virtualized'
 import { sortByProperty } from '../../lib/sort'
 
@@ -45,7 +45,7 @@ export const PinningManager = ({ pinningServices, doFetchPinningServices, doPins
   }, [doFetchPinningServices])
 
   const localPinning = useMemo(() =>
-    ({ name: t('localPinning'), type: 'LOCAL', svgIcon: GlyphPin, totalSize: pinsSize, numberOfPins }),
+    ({ name: t('localPinning'), type: 'LOCAL', totalSize: pinsSize, numberOfPins }),
   [numberOfPins, pinsSize, t])
 
   const sortedServices = useMemo(() =>
@@ -53,7 +53,6 @@ export const PinningManager = ({ pinningServices, doFetchPinningServices, doPins
   [pinningServices, sortSettings.sortBy, sortSettings.sortDirection])
 
   const sortedList = useMemo(() => [localPinning, ...sortedServices], [localPinning, sortedServices])
-
   return (
     <Fragment>
       <div className="mv4 pinningManager">
@@ -74,7 +73,7 @@ export const PinningManager = ({ pinningServices, doFetchPinningServices, doPins
                 sortBy={sortSettings.sortBy}
                 sortDirection={sortSettings.sortDirection}>
                 <Column label={t('service')} title={t('service')} dataKey='name' width={width * 0.4} flexShrink={0} flexGrow={1} cellRenderer={ServiceCell} className='charcoal truncate f6' />
-                <Column label={t('size')} title={t('size')} dataKey='totalSize' width={width * 0.2} flexShrink={0} cellRenderer={({ rowData }) => <SizeCell rowData={rowData} t={t}/>} className='charcoal truncate f6 pl2' />
+                {/* <Column label={t('size')} title={t('size')} dataKey='totalSize' width={width * 0.2} flexShrink={0} cellRenderer={({ rowData }) => <SizeCell rowData={rowData} t={t}/>} className='charcoal truncate f6 pl2' /> */}
                 <Column label={t('pins')} title={t('pins')} dataKey='numberOfPins' width={width * 0.2} flexShrink={1} cellRenderer={({ rowData }) => <NumberOfPinsCell rowData={rowData} t={t}/>} className='charcoal truncate f6 pl2' />
                 <Column label={t('autoUpload')} title={t('autoUpload')} dataKey='autoUpload' width={width * 0.2} flexShrink={1} cellRenderer={({ rowData }) => <AutoUploadCell autoUpload={rowData.autoUpload} type={rowData.type} name={rowData.name} doRemovePinningService={doRemovePinningService} t={t} />} className='pinningManagerColumn charcoal truncate f6 pl2' />
               </Table>
@@ -89,7 +88,10 @@ export const PinningManager = ({ pinningServices, doFetchPinningServices, doPins
       </div>
 
       <Overlay show={isModalOpen} onLeave={onModalClose}>
-        <PinningModal className='outline-0' onLeave={onModalClose} t={t} />
+        <PinningModal className='outline-0' onLeave={() => {
+          onModalClose()
+          doFetchPinningServices()
+        }} t={t} />
       </Overlay>
     </Fragment>
   )
@@ -99,25 +101,38 @@ PinningManager.defaultProps = {
   pinningServices: []
 }
 
-const ServiceCell = ({ rowData }) => {
-  const iconClass = 'mr2 pr1 fill-aqua flex-shrink-0'
-  return (
-    <div className='flex items-center' title={rowData.name}>
-      { rowData.svgIcon && (<rowData.svgIcon width="28" height="28" className={iconClass} />)}
-      { rowData.icon && (<img src={rowData.icon} alt={rowData.name} width="28" height="28" className={iconClass} style={{ objectFit: 'contain' }} />)}
-      <span className="truncate">{ rowData.name }</span>
-    </div>
-  )
+const getIcon = (rowData) => {
+  const colors = ['aqua', 'navy', 'gray', 'charcoal', 'red', 'yellow', 'teal', 'green', 'link']
+  const color = rowData.type === 'LOCAL' ? colors[0] : colors[Math.floor(Math.random() * colors.length)]
+  const iconClass = `mr2 pr1 fill-${color} flex-shrink-0`
+
+  if (rowData.icon) {
+    return <img src={rowData.icon} alt={rowData.name} width="28" height="28" className={iconClass} style={{ objectFit: 'contain' }} />
+  }
+  return <GlyphPin width="28" height="28" className={iconClass} />
 }
 
-const SizeCell = ({ rowData, t }) => (
-  <p className={ !rowData.totalSize ? 'gray nowrap' : 'nowrap'}>{ !rowData.totalSize
-    ? `${(t('app:terms:loading'))}...`
-    : filesize(rowData.totalSize || 0, {
-      round: rowData.totalSize >= 1000000000 ? 1 : 0, spacer: ''
-    })}</p>
+const ServiceCell = ({ rowData }) => (
+  <div className='flex items-center' title={rowData.name}>
+    {getIcon(rowData)}
+    <span className="truncate">{ rowData.name }</span>
+  </div>
 )
-const NumberOfPinsCell = ({ rowData, t }) => (<div className={!rowData.numberOfPins ? 'gray' : ''}>{rowData.numberOfPins || `${(t('app:terms:loading'))}...`}</div>)
+
+// const SizeCell = ({ rowData, t }) => (
+//   <p className={ !rowData.totalSize ? 'gray nowrap' : 'nowrap'}>{ !rowData.totalSize
+//     ? `${(t('app:terms:loading'))}...`
+//     : filesize(rowData.totalSize || 0, {
+//       round: rowData.totalSize >= 1000000000 ? 1 : 0, spacer: ''
+//     })}</p>
+// )
+const NumberOfPinsCell = ({ rowData, t }) => {
+  const { numberOfPins } = rowData
+  if (numberOfPins === 'Error') {
+    return <div className='red'>{t('errors.failedToFetch')}</div>
+  }
+  return <div className={rowData.numberOfPins >= 0 ? '' : 'gray'}>{rowData.numberOfPins >= 0 ? rowData.numberOfPins : `${(t('app:terms:loading'))}...`}</div>
+}
 const AutoUploadCell = ({ autoUpload, name, doRemovePinningService, t, type }) => (
   <div className="flex justify-between items-center">
     <div className={!autoUpload ? 'gray' : ''}>{ autoUpload ? t('autoUploadKeys.' + autoUpload) : 'N/A' }</div>
@@ -134,6 +149,11 @@ const OptionsCell = ({ doRemovePinningService, name, t }) => {
     setContextVisibility(false)
   }
 
+  const visitServicesList = {
+    pinata: 'https://webtest.pinata.cloud/documentation'
+  }
+  const visitServiceUrl = visitServicesList[name.toLowerCase()]
+
   return (
     <div>
       <button className="button-inside-focus" onClick={() => setContextVisibility(true)} ref={buttonRef} aria-label={t('showOptions')}>
@@ -141,12 +161,13 @@ const OptionsCell = ({ doRemovePinningService, name, t }) => {
       </button>
       <ContextMenu className="pv2 ph1" style={{ zIndex: 1001 }} visible={isContextVisible}
         target={buttonRef} onDismiss={() => setContextVisibility(false)} arrowAlign="right">
-        <ContextMenuItem className='pv2 ph1' onClick={ /* TODO: add this feature */ () => setContextVisibility(false) }>
-          <StrokeSettings width="28" className='fill-aqua'/> <span className="ph1">{t('edit')}</span>
-        </ContextMenuItem>
-        <ContextMenuItem className='pv2 ph1' onClick={ /* TODO: add this feature */ () => setContextVisibility(false) }>
-          <StrokeExternalLink width="28" className='fill-aqua'/> <span className="ph1">{t('visitService')}</span>
-        </ContextMenuItem>
+        { visitServiceUrl && (
+          <ContextMenuItem className='pv2 ph1' onClick={ () => setContextVisibility(false) }>
+            <a className='link flex items-center' href={visitServiceUrl} target='_blank' rel='noopener noreferrer'>
+              <StrokeExternalLink width="28" className='fill-aqua'/> <span className="ph1 charcoal">{t('visitService')}</span>
+            </a>
+          </ContextMenuItem>)
+        }
         <ContextMenuItem className='pv2 ph1' onClick={ handleRemove }>
           <StrokeCancel width="28" className='fill-aqua'/> <span className="ph1">{t('remove')}</span>
         </ContextMenuItem>
