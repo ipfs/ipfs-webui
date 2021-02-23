7da7ca92cca5fd4bae02c384851514885a59a054
Irakli Gozalishvili <contact@gozala.io>
2021-02-22 21:19:34 -0800

    fix: more dep inconsistencies that coused failures

--- test/helpers/cid.js
+++ test/helpers/cid.js
@@ -6,5 +6,5 @@ import Crypto from 'crypto'
 export async function fakeCid () {
   const bytes = await promisify(Crypto.randomBytes)(Math.round(Math.random() * 1000))
   const mh = await multihashing(bytes, 'sha2-256')
-  return new CID(0, 'dag-pb', mh)
+  return new CID(0, 'dag-pb', Uint8Array.from(mh))
 }
