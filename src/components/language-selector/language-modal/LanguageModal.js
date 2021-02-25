59847150aacaab2f58ee53f73531582b951dddde
Marcin Rataj <lidel@lidel.org>
2021-02-25 22:34:06 +0100

    chore(i18n): dep bump + automatic locale data
    Small i18n refactor that  makes things more future-proof:
    https://github.com/ipfs-shipyard/i18n/issues/9

--- src/components/language-selector/language-modal/LanguageModal.js
+++ src/components/language-selector/language-modal/LanguageModal.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import i18n, { localesList } from '../../../i18n'
-import { getLanguage } from '../../../lib/i18n'
 
 // Components
 import { Modal, ModalBody, ModalActions } from '../../modal/Modal'
@@ -21,10 +20,10 @@ const LanguageModal = ({ t, tReady, onLeave, link, className, ...props }) => {
         <div className='pa2 flex flex-wrap'>
           { localesList.map((lang) =>
             <button
-              key={`lang-${lang}`}
+              key={`lang-${lang.locale}`}
               className='pa2 w-33 flex nowrap bg-transparent bn outline-0 blue justify-center'
-              onClick={() => handleClick(lang)}>
-              { getLanguage(lang) }
+              onClick={() => handleClick(lang.locale)}>
+              { lang.nativeName }
             </button>
           )}
         </div>
