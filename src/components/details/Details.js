f06350e2fc69c6e63d9794b4c751b572f21c8551
Rafael Ramalho <rafazelramalho19@gmail.com>
2021-04-01 01:38:02 +0100

    fix: text selection inside advanced section (#1747)

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -4,11 +4,12 @@ import 'details-polyfill'
 const Details = ({
   summaryText = 'Advanced',
   children,
+  onClick,
   ...props
 }) => {
   return (
     <details {...props}>
-      <summary className='pointer blue outline-0'>{summaryText}</summary>
+      <summary className='pointer blue outline-0' onClick={onClick}>{summaryText}</summary>
       {children}
     </details>
   )
