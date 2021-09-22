3910e86fd90e0a958338efe961219dfd2ed04375
Henrique Dias <hacdias@gmail.com>
2021-09-22 21:08:17 +0200

    feat: "import from IPFS" with a name (#1853)
    * feat: "import from IPFS" with a name
    * style: tweak placeholders
    * refactor: simplified validation
    
    - removed smarts related to ?filename=
      (we dont accept URLs so it made no sense)
    - removed trim from path field to fix import of paths with whitespace
    - added trim to filename to avoid surprises
    
    Co-authored-by: Marcin Rataj <lidel@lidel.org>

--- src/files/modals/Modals.js
+++ src/files/modals/Modals.js
@@ -52,8 +52,8 @@ class Modals extends React.Component {
     command: 'ipfs --help'
   }
 
-  onAddByPath = (path) => {
-    this.props.onAddByPath(path)
+  onAddByPath = (path, name) => {
+    this.props.onAddByPath(path, name)
     this.leave()
   }
 
