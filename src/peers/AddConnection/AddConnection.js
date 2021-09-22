40a0ce536cb6a9c63b1fdb89f261b96e3fd391b8
Henrique Dias <hacdias@gmail.com>
2021-09-23 00:01:12 +0200

    feat: add peer permanently by default (#1850)
    * feat: add peer permanently by default
    * fix: reuse peer and force config reload
    
    This will reuse any pre-existing peer entry (add addr to existing one)
    and will force Setting screen to pick up changes made to Peering.Peers
    section of the config.
    
    * style: switch to Checkbox
    + use bootstrapper as example
    
    Co-authored-by: Marcin Rataj <lidel@lidel.org>

--- src/peers/AddConnection/AddConnection.js
+++ src/peers/AddConnection/AddConnection.js
@@ -3,16 +3,21 @@ import { withTranslation } from 'react-i18next'
 import { connect } from 'redux-bundler-react'
 import isIPFS from 'is-ipfs'
 
+import Checkbox from '../../components/checkbox/Checkbox'
 import Icon from '../../icons/StrokeDecentralization'
 import Button from '../../components/button/Button'
 import Overlay from '../../components/overlay/Overlay'
-import TextInputModal from '../../components/text-input-modal/TextInputModal'
+
+import ComponentLoader from '../../loader/ComponentLoader'
+import { Modal, ModalActions, ModalBody } from '../../components/modal/Modal'
 
 class AddConnection extends React.Component {
   state = {
     open: false,
     loading: false,
-    hasErrored: false
+    isValid: false,
+    permanent: true,
+    maddr: ''
   }
 
   toggleModal = () => {
@@ -21,37 +26,72 @@ class AddConnection extends React.Component {
     })
   }
 
-  addConnection = async (maddr) => {
+  onPeeringToggle = () => {
+    this.setState({ permanent: !this.state.permanent })
+  }
+
+  onChange = (event) => {
+    const { value, name } = event.target
+    this.setState({ [name]: value })
+    if (name === 'maddr') {
+      this.setState({ isValid: isIPFS.peerMultiaddr(value) })
+    }
+  }
+
+  onSubmit = async () => {
+    if (!this.state.isValid) {
+      return
+    }
+
     this.setState({ loading: true })
-    const { type } = await this.props.doConnectSwarm(maddr)
-    this.setState({ loading: false, hasErrored: true })
+    const { maddr, permanent } = this.state
+    const { type } = await this.props.doConnectSwarm(maddr, permanent)
+    const errored = type === 'SWARM_CONNECT_FAILED'
+    this.setState({ loading: false, isValid: !errored })
 
-    if (type === 'SWARM_CONNECT_FAILED') return
-
+    if (errored) return
+    this.setState({ isValid: false, permanent: true, maddr: '' })
     this.toggleModal()
   }
 
-  onInputChange = () => {
-    if (!this.state.hasErrored) return
-
-    this.setState({ hasErrored: false })
+  onKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      this.onSubmit()
+    }
   }
 
-  getDescription = () => {
+  get inputClass () {
+    if (this.state.maddr === '') {
+      return ''
+    }
+
+    if (this.state.isValid) {
+      return 'b--green-muted focus-outline-green'
+    }
+
+    return 'b--red-muted focus-outline-red'
+  }
+
+  get isDisabled () {
+    return this.state.maddr === '' || !this.state.isValid
+  }
+
+  get description () {
     const { t } = this.props
-    const codeClass = 'w-90 mb1 pa1 bg-snow f7 charcoal-muted truncate'
+    const codeClass = 'w-90 mb1 pa1 dib bg-snow f7 charcoal-muted truncate'
 
     return (
       <div className='mb3 flex flex-column items-center'>
-        <p className='gray w-80'>{t('insertPeerAddress')}</p>
+        <p className='charcoal w-80'>{t('insertPeerAddress')}</p>
         <span className='w-80 mv2 f7 charcoal-muted'>{t('app:terms.example')}</span>
-        <code className={codeClass}>/ip4/76.176.168.65/tcp/4001/p2p/QmbBHw1Xx9pUpAbrVZUKTPL5Rsph5Q9GQhRvcWVBPFgGtC</code>
+        <code className={codeClass}>/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN</code>
+        <code className={codeClass}>/ip4/147.75.109.213/udp/4001/quic/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN</code>
       </div>
     )
   }
 
   render () {
-    const { open, loading, hasErrored } = this.state
+    const { open, loading } = this.state
     const { t } = this.props
 
     return (
@@ -61,18 +101,38 @@ class AddConnection extends React.Component {
         </Button>
 
         <Overlay show={open} onLeave={this.toggleModal}>
-          <TextInputModal
-            validate={isIPFS.peerMultiaddr}
-            onSubmit={this.addConnection}
-            onCancel={this.toggleModal}
-            onInputChange={this.onInputChange}
-            submitText={t('app:actions.add')}
-            Icon={Icon}
-            title={t('addConnection')}
-            description={this.getDescription()}
-            loading={loading}
-            error={hasErrored}
-          />
+          <Modal onCancel={this.toggleModal}>
+            <ModalBody title={t('addConnection')} Icon={Icon}>
+              { this.description }
+
+              <input
+                name='maddr'
+                type='text'
+                value={this.state.maddr}
+                onChange={this.onChange}
+                onKeyPress={this.onKeyPress}
+                required
+                className={`input-reset charcoal ba b--black-20 br1 pa2 mv2 db w-90 center focus-outline ${this.inputClass}`} />
+
+              <Checkbox
+                className='dib'
+                onChange={this.onPeeringToggle}
+                checked={this.state.permanent}
+                label={<span className='f6 charcoal-muted lh-copy'>{t('addPermanentPeer')}</span>}/>
+            </ModalBody>
+
+            <ModalActions>
+              <Button className='ma2 tc' bg='bg-gray' onClick={this.toggleModal}>{t('actions.cancel')}</Button>
+              <Button className='ma2 tc' bg='bg-teal' disabled={this.isDisabled} onClick={this.onSubmit}>{t('app:actions.add')}</Button>
+            </ModalActions>
+
+            { loading && (
+              <div className="flex items-center justify-center absolute top-0 left-0 right-0 bottom-0">
+                <div className="absolute top-0 left-0 right-0 bottom-0 bg-light-gray o-80"/>
+                <ComponentLoader style={{ width: '50%', margin: 'auto' }} />
+              </div>
+            ) }
+          </Modal>
         </Overlay>
       </div>
     )
