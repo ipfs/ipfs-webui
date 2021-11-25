1b65411a95e32661a6cde57e8847e3975969e575
Marcin Rataj <lidel@lidel.org>
2021-11-25 02:02:31 +0100

    style: ipns publishing modal tweaks

--- src/files/modals/publish-modal/PublishModal.js
+++ src/files/modals/publish-modal/PublishModal.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { withTranslation } from 'react-i18next'
 import Button from '../../../components/button/Button'
 import { Modal, ModalActions, ModalBody } from '../../../components/modal/Modal'
+import Icon from '../../../icons/StrokeSpeaker'
 import { connect } from 'redux-bundler-react'
 import Radio from '../../../components/radio/Radio'
 import './PublishModal.css'
@@ -20,11 +21,11 @@ const PublishModal = ({ t, tReady, onCancel, onSubmit, file, ipnsKeys, className
 
   return (
     <Modal {...props} className={className} onCancel={onCancel} >
-      <ModalBody title={t('publishModal.title')}>
-        <p>{t('publishModal.toPublish')} <span className='f6 monospace'>{file.cid.toString()}</span></p>
-
+      <ModalBody Icon={Icon} title={t('publishModal.title')}>
+        <div className='tl pv3'>{t('publishModal.cidToPublish')} <span className='f6 charcoal-muted monospace'>{file.cid.toString()}</span></div>
+        <div className='tl pv3'>{t('publishModal.publishUnderKey')}</div>
         <div className="publishModalKeys">
-          { ipnsKeys.map(({ name }) => (
+          { ipnsKeys.map(({ name, id }) => (
             <button className="flex items-center pa1 hoverable-button" key={name} onClick={() => setSelectedKey(name)}>
               <Radio className='pv3 pl3 pr1 flex-none' checked={name === selectedKey} label={name} style={{ pointerEvents: 'none' }} />
             </button>
