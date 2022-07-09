1a04511b1c80b62a264cc11e0d7c494ca9fef00c
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-07-09 01:32:27 -0700

    chore(deps): fix eslint
    thanks tjaddison!
    @see https://tjaddison.com/blog/2021/03/updating-babel-eslint-to-babeleslint-parser-for-react-apps/

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react']
+    }
+  },
   extends: ['react-app', 'standard', 'plugin:jsx-a11y/recommended'],
   plugins: ['jsx-a11y'],
   // ignore .ts files because it fails to parse it.
