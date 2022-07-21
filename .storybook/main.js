f637913fee5ad20577228335cc950cccd569b911
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-07-21 09:14:28 -0700

    chore: migrate addons to .storybook/main.js

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+/**
+ * @see https://github.com/storybookjs/storybook/blob/master/MIGRATION.md#from-version-52x-to-53x
+ */
+
+module.exports = {
+  stories: ['../**/*.stories.js'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-a11y',
+    '@storybook/addon-knobs',
+  ],
+};
