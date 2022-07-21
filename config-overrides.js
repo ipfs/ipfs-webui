49fc1ee6f7ae069eb03a8885ffba4ef5ffc7a822
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-07-20 21:23:05 -0700

    fix: webpack fallbacks

--- config-overrides.js
+++ config-overrides.js
@@ -11,14 +11,10 @@ module.exports = function override(config) {
   const fallback = config.resolve.fallback || {};
 
   Object.assign(fallback, {
-    "crypto": require.resolve("crypto-browserify"),
-    "stream": require.resolve("stream-browserify"),
-    "assert": require.resolve("assert"),
-    "http": require.resolve("stream-http"),
-    "https": require.resolve("https-browserify"),
-    "os": require.resolve("os-browserify"),
-    "url": require.resolve("url"),
-    "path": require.resolve("path-browserify"),
+    "assert": require.resolve('./src/webpack-fallbacks/assert'),
+    "stream": require.resolve('./src/webpack-fallbacks/stream'),
+    "os": require.resolve('./src/webpack-fallbacks/os'),
+    "path": require.resolve('./src/webpack-fallbacks/path'),
   })
 
   config.resolve.fallback = fallback;
