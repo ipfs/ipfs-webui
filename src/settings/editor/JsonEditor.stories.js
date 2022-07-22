9767c2dcf1f4864af9a4454e4ce7faa9b5010767
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-07-21 19:14:07 -0700

    chore: update JsonEditor.stories.js

--- src/settings/editor/JsonEditor.stories.js
+++ src/settings/editor/JsonEditor.stories.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { checkA11y } from '@storybook/addon-a11y'
-
 // Components
 import JsonEditor from './JsonEditor'
 
 // Fixtures
 import config from './fixtures/example-config.json'
 
-storiesOf('JSON Editor', module)
-  .addDecorator(checkA11y)
-  .add('Default', () => (
-    <JsonEditor value={JSON.stringify(config, null, 2)} />
-  ))
+/**
+ * @type {import('@storybook/react').Meta}
+ */
+const JsonEditorStory = {
+  title: 'JSON Editor',
+  component: JsonEditor,
+  args: {
+    value: JSON.stringify(config, null, 2),
+    readOnly: false
+  }
+}
+
+export default JsonEditorStory
+
+export const Default = {}
