c220df4d0078da2bd249f9fb538689686ab3744f
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-07-21 19:13:26 -0700

    fix: default storybook argTypes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,14 +8,33 @@ import getStore from '../src/bundles'
 import i18n from '../src/i18n'
 import DndBackend from '../src/lib/dnd-backend'
 
-export const decorators = [
-  (Story) => (
-    <Provider store={getStore(undefined)}>
-      <I18nextProvider i18n={i18n} >
-        <DndProvider backend={DndBackend}>
-          <Story />
-        </DndProvider>
-      </I18nextProvider>
-    </Provider>
-  )
-];
+/**
+ * @type {import('@storybook/addons').BaseAnnotations}
+ */
+const baseAnnotations = {
+  decorators: [
+    (Story) => (
+      <Provider store={getStore(undefined)}>
+        <I18nextProvider i18n={i18n} >
+          <DndProvider backend={DndBackend}>
+            <Story />
+          </DndProvider>
+        </I18nextProvider>
+      </Provider>
+    )
+  ],
+  /**
+   * @see https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
+   */
+  argTypes: {
+    onChange: {
+      action: 'changed'
+    },
+    onClick: {
+      action: 'clicked'
+    }
+  }
+}
+
+export const decorators = baseAnnotations.decorators
+export const argTypes = baseAnnotations.argTypes
