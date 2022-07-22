ecca78e9ce1f18ea884eeb1f58ea9456688618f5
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-07-22 07:25:44 -0700

    fix: Cid.stories.js

--- src/components/cid/Cid.stories.js
+++ src/components/cid/Cid.stories.js
@@ -1,7 +1,3 @@
-// import React from '@storybook/react'
-// import { storiesOf } from '@storybook/react'
-// import { action } from '@storybook/addon-actions'
-// import { checkA11y } from '@storybook/addon-a11y'
 import Cid from './Cid'
 
 /**
@@ -13,48 +9,42 @@ const CidStory = {
   parameters: {
     actions: {
       disable: false,
-      handles: ['click']
     },
-    argTypes: {
-      onClick: { action: 'clicked' }
-    },
-    args: {
-      value: 'QmYPNmahJAvkMTU6tDx5zvhEkoLzEFeTDz6azDCSNqzKkW',
-      className: 'db ma2 monospace',
-      identicon: false
-    }
+  },
+  argTypes: {
+    onClick: { action: 'clicked' }
   }
 }
 export default CidStory
 
-const Template = (args) => (
-  <Cid className={args.className} value={args.value} identicon={args.identicon} />
-)
-
-export const CidV0 = Template.bind({})
-CidV0.args = {
-  className: 'db ma2 monospace',
-  value: 'QmYPNmahJAvkMTU6tDx5zvhEkoLzEFeTDz6azDCSNqzKkW',
-  identicon: false
+export const CidV0 = {
+  args: {
+    className: 'db ma2 monospace',
+    value: 'QmYPNmahJAvkMTU6tDx5zvhEkoLzEFeTDz6azDCSNqzKkW',
+    identicon: false
+  }
 }
 
-export const CidV0Identicon = Template.bind({})
-CidV0Identicon.args = {
-  className: 'db ma2 monospace',
-  value: 'QmYPNmahJAvkMTU6tDx5zvhEkoLzEFeTDz6azDCSNqzKkW',
-  identicon: true
+export const CidV0Identicon = {
+  args: {
+    className: 'db ma2 monospace',
+    value: 'QmYPNmahJAvkMTU6tDx5zvhEkoLzEFeTDz6azDCSNqzKkW',
+    identicon: true
+  }
 }
 
-export const CidV1 = Template.bind({})
-CidV1.args = {
-  className: 'db ma2 monospace',
-  value: 'zb2rhZMC2PFynWT7oBj7e6BpDpzge367etSQi6ZUA81EVVCxG',
-  identicon: false
+export const CidV1 = {
+  args: {
+    className: 'db ma2 monospace',
+    value: 'zb2rhZMC2PFynWT7oBj7e6BpDpzge367etSQi6ZUA81EVVCxG',
+    identicon: false
+  }
 }
 
-export const CidV1Sha3 = Template.bind({})
-CidV1Sha3.args = {
-  className: 'db ma2 monospace',
-  value: 'zB7NbGN5wyfSbNNNwo3smZczHZutiWERdvWuMcHXTj393RnbhwsHjrP7bPDRPA79YWPbS69cZLWXSANcwUMmk4Rp3hP9Y',
-  identicon: false
+export const CidV1Sha3 = {
+  args: {
+    className: 'db ma2 monospace',
+    value: 'zB7NbGN5wyfSbNNNwo3smZczHZutiWERdvWuMcHXTj393RnbhwsHjrP7bPDRPA79YWPbS69cZLWXSANcwUMmk4Rp3hP9Y',
+    identicon: false
+  }
 }
