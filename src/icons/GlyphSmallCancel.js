741db121f5a3bb625ee81d70be78e0450e1f0d49
Henrique Dias <hacdias@gmail.com>
2022-08-18 14:38:39 +0200

    feat: new ipfs-css with new failed pin icon

--- src/icons/GlyphSmallCancel.js
+++ src/icons/GlyphSmallCancel.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import * as React from 'react'
 
-const GlyphSmallCancel = props => (
-  <svg viewBox='0 0 100 100' {...props}>
-    <path d='M63.79 63.2l-3.88 3.88-9.71-9.7-9.7 9.7-3.88-3.88 9.7-9.71-9.7-9.7 3.88-3.88 9.7 9.7 9.71-9.7 3.88 3.88-9.7 9.7 9.7 9.71z' />
-  </svg>
-)
+function SvgGlyphSmallCancel (props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" {...props}>
+      <path d="M63.79 63.2l-3.88 3.88-9.71-9.7-9.7 9.7-3.88-3.88 9.7-9.71-9.7-9.7 3.88-3.88 9.7 9.7 9.71-9.7 3.88 3.88-9.7 9.7 9.7 9.71z" />
+    </svg>
+  )
+}
 
-export default GlyphSmallCancel
+export default SvgGlyphSmallCancel
