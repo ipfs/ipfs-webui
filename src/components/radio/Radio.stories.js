2613befcc904df527ad3667f0d8806033fced310
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-08-26 13:12:04 -0700

    fix: run eslint --fix

--- src/components/radio/Radio.stories.js
+++ src/components/radio/Radio.stories.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { checkA11y } from '@storybook/addon-a11y';
-import Radio from './Radio';
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import { checkA11y } from '@storybook/addon-a11y'
+import Radio from './Radio'
 
 const bigPicture = {
-    transform: 'scale(5)',
-    transformOrigin: 'top left',
-};
+  transform: 'scale(5)',
+  transformOrigin: 'top left'
+}
 
 export default {
-    title: 'Radio',
-    decorators: [checkA11y],
-};
+  title: 'Radio',
+  decorators: [checkA11y]
+}
 
 export const Default = () => (
     <div>
         <Radio className="ma2" label="Click me!" onChange={action('Checked')} />
     </div>
-);
+)
 
 export const Disabled = () => (
     <div>
         <Radio label="Click me!" className="ma2" disabled onChange={action('Checked')} />
     </div>
-);
+)
 
 export const Big = () => (
     <div>
         <Radio style={bigPicture} label="Click me!" className="ma2" onChange={action('Checked')} />
     </div>
-);
+)
--- src/files/modals/publish-modal/PublishModal.stories.js
+++ src/files/modals/publish-modal/PublishModal.stories.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import i18n from '../../../i18n';
-import { PublishModal } from './PublishModal';
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import i18n from '../../../i18n'
+import { PublishModal } from './PublishModal'
 
 export default {
-    title: 'Files/Modals',
-};
+  title: 'Files/Modals'
+}
 
 export const Publishing = () => (
     <div className="ma3">
         <PublishModal t={i18n.getFixedT('en', 'files')} onCancel={action('Cancel')} onSubmit={action('Publish')} />
     </div>
-);
+)
