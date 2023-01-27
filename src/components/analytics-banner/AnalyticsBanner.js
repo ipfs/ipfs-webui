cac766378d75556e37ef7082bc3bc560748f8e97
Dan McQuillan <djmcquillan76@gmail.com>
2023-01-26 16:13:05 -0800

    fix: webui update metrics to opt-out by default (part of 2074) (#2084)
    * fix: webui update metrics to opt-out by default
    
    * fix: webui update metrics to opt-out by default moved code to analytics init
    
    * fix: webui update metrics to be defaulted on for users previously opted out and updated tests
    
    * fix: webui update analytics per pr removed comments
    
    * fix: webui update analytics status label typo and changed ask to enable banner logic
    
    * fix: webui updates to linter and typo in actions
    
    * fix: opt-out metrics up major refactor, renaming and tests based on reviews
    
    * fix: opt-out metrics 2074 removed console logs
    
    * fix: opt-out metrics 2074/2085 updates to tests
    
    * fix: opt-out metrics 2074 update to tests and selectAnalyticsOptedOutPriorToDefaultOptIn
    
    * fix: opt-out metrics 2074 update to datetimes in tests
    
    * fix: opt-out metrics 2074 reverted lastDisabledAt and lastEnabledAt changes
    
    * fix: opt-out metrics removed unnecessary render test
    
    Co-authored-by: Dan McQuillan <daniel.mcquillan@protocol.ai>

---
+++ src/components/analytics-banner/AnalyticsBanner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from '../button/Button'
+
+const AnalyticsBanner = ({ className, label, yesLabel, onYes }) => {
+  return (
+    <div className={`f6 pv3 pv2-ns ph3 tc bg-snow charcoal flex ${className}`}>
+      <span className='fw4 lh-copy dib mb2 tl'>
+        {label}
+      </span>
+      <span className='dib'>
+        <Button className='ml3 mv1 tc' bg={'bg-green'} onClick={onYes}>{yesLabel}</Button>
+      </span>
+    </div>
+  )
+}
+
+export default AnalyticsBanner
