e1f029618af53662884f01e9e5c0b4d450d0ff09
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-01-26 18:37:43 -0800

    chore(env.js): add tests

--- src/env.js
+++ src/env.js
@@ -1,4 +1,3 @@
-
 /**
  * @see https://github.com/ipfs/ipfs-webui/issues/2078#issuecomment-1384444232
  * @see https://github.com/ipfs/ipfs-webui/issues/2078#issuecomment-1384605253
@@ -6,26 +5,25 @@
  */
 
 const webuiRegex = /(?:webui-ipfs|webui\.ipfs)/
-const localhostRegex = /localhost/
+const localhostRegex = /(?:localhost|127.0.0.1)/
+const ipfsRegex = /ipfs/
 export async function getDeploymentEnv () {
   const { origin } = globalThis.location
   if (webuiRegex.test(origin)) {
-    if (localhostRegex.test(origin)) {
-      return 'local'
-    }
-    return 'webui.ipfs'
+    return localhostRegex.test(origin) ? 'local' : 'webui.ipfs'
   }
   try {
     const response = await fetch('/webui', { method: 'HEAD' })
-    if (response.redirected) {
+    const { pathname } = new URL(response.url)
+    if (response.redirected && ipfsRegex.test(pathname)) {
       /**
        * @see https://github.com/ipfs/kubo/blob/f54b2bcf6061219f7f481e8660e6707ae3bc3c38/cmd/ipfs/daemon.go#L677
        * @see https://github.com/ipfs/kubo/blob/15093a00116ecf6b550023155f31a33f4bba6403/core/corehttp/webui.go#L57
        */
       return 'kubo'
     }
-    return 'local'
   } catch {
-    return 'webui.ipfs'
+    // dont throw from this method
   }
+  return localhostRegex.test(origin) ? 'local' : 'webui.ipfs'
 }
---
+++ src/env.test.js
@@ -0,0 +1,49 @@
+/* global describe, it, expect, afterEach, jest */
+// @ts-check
+import { getDeploymentEnv } from './env'
+
+describe('env.js', function () {
+  describe('getDeploymentEnv', function () {
+    const fetchMock = jest.fn()
+    function testOriginAndEnv (url, fetchImpl, expectedEnv, numberOfFetchCalls) {
+      it(`${url} returns env ${expectedEnv}`, async () => {
+        const { origin } = new URL(url)
+        // @ts-expect-error
+        jest.spyOn(globalThis, 'location', 'get').mockReturnValue({ origin })
+        fetchMock.mockImplementation(fetchImpl)
+        expect(await getDeploymentEnv()).toBe(expectedEnv)
+        expect(fetchMock.mock.calls.length).toBe(numberOfFetchCalls)
+      })
+    }
+    globalThis.fetch = fetchMock
+
+    afterEach(function () {
+      jest.clearAllMocks()
+    })
+    /**
+     * webui.ipfs deployed endpoints
+     */
+    testOriginAndEnv('https://webui.ipfs.io', () => {}, 'webui.ipfs', 0)
+    testOriginAndEnv('https://webui-ipfs-io.ipns.dweb.link/', () => {}, 'webui.ipfs', 0)
+    testOriginAndEnv('https://webui.ipfs.tech', () => {}, 'webui.ipfs', 0)
+    testOriginAndEnv('https://some-random-url', () => { throw new Error('no match on origin') }, 'webui.ipfs', 1)
+    testOriginAndEnv('https://some-random-url', () => ({ redirected: false, url: 'https://some-random-url/webui' }), 'webui.ipfs', 1)
+
+    /**
+     * local webui endpoints
+     */
+    testOriginAndEnv('http://webui.ipfs.io.ipns.localhost:8080/', () => {}, 'local', 0)
+    testOriginAndEnv('http://webui-ipfs.io.ipns.localhost:8080/', () => {}, 'local', 0)
+    testOriginAndEnv('http://webui.ipfs.io.ipns.localhost:48080/', () => {}, 'local', 0) // brave
+    testOriginAndEnv('http://localhost:3000/', () => { throw new Error('no match on origin') }, 'local', 1)
+    testOriginAndEnv('http://127.0.0.1:3000/', () => { throw new Error('no match on origin') }, 'local', 1)
+    testOriginAndEnv('https://some-random-url.localhost:3333', () => ({ redirected: false, url: 'https://some-random-url.localhost:3333/webui' }), 'local', 1)
+
+    /**
+     * Kubo webui endpoints
+     */
+    testOriginAndEnv('http://localhost:5001/webui', () => ({ redirected: true, url: 'http://localhost:5001/ipfs/bafybeiequgo72mrvuml56j4gk7crewig5bavumrrzhkqbim6b3s2yqi7ty/' }), 'kubo', 1)
+    testOriginAndEnv('http://127.0.0.1:5002/webui', () => ({ redirected: true, url: 'http://127.0.0.1:5002/ipfs/bafybeiequgo72mrvuml56j4gk7crewig5bavumrrzhkqbim6b3s2yqi7ty/' }), 'kubo', 1)
+    testOriginAndEnv('https://some-random-url.localhost:3333', () => ({ redirected: true, url: 'https://some-random-url.localhost:3333/ipfs/bafybeiequgo72mrvuml56j4gk7crewig5bavumrrzhkqbim6b3s2yqi7ty/' }), 'kubo', 1)
+  })
+})
