b3db8acc7b7f36c3f77f22ab408def8019cec4d6
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-02-23 11:53:14 -0800

    fix: current language displays correctly for fallbacks

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -9,7 +9,23 @@ export const getCurrentLanguage = () => {
 export const getLanguage = (localeCode) => {
   if (!localeCode) return 'Unknown'
 
-  const info = languages[localeCode]
+  let info = languages[localeCode]
+  const fallbackLanguages = i18n.options.fallbackLng[localeCode]
+
+  if (info == null && fallbackLanguages != null) {
+    /**
+     * check fallback languages before attempting to split a 'lang-COUNTRY' code
+     * fixed issue with displaying 'English' when i18nLng is set to 'ko'
+     * discovered when looking into https://github.com/ipfs/ipfs-webui/issues/2097
+     */
+    const fallback = fallbackLanguages
+    for (const locale of fallback) {
+      info = languages[locale]
+      if (info != null) {
+        return info.nativeName
+      }
+    }
+  }
 
   if (!info) {
     // if we haven't got the info in the `languages.json` we split it to get the language
