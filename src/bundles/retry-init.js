b8cf74aa30ce38090fc345b091276f18076d9bb2
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-06-30 18:35:02 -0700

    fix: dont spam IPFS_INIT_FAILED events to countly (#2133)
    * fix: dont spam IPFS_INIT_FAILED events to countly
    
    * fix(hof/functions): jsdoc types are valid
    
    * fix(hofs/functions): types and add debounce function
    
    * chore: cleanup code
    
    * fix: typecheck
    
    see https://github.com/ipfs/ipfs-webui/actions/runs/5418218369/jobs/9850182385\?pr\=2133
    
    * test(hofs/functions): add tests
    
    * fix: typecheck succeeds
    
    * feat: fn first hofs, add param guards
    
    * chore: wrap check for limited addEvent

--- src/bundles/retry-init.js
+++ src/bundles/retry-init.js
@@ -2,17 +2,46 @@ import { createSelector } from 'redux-bundler'
 import { ACTIONS } from './ipfs-provider.js'
 
 /**
- * @typedef {import('./ipfs-provider').Message} Message
+ *
+ * @typedef {Object} AppIdle
+ * @property {'APP_IDLE'} type
+ *
+ * @typedef {Object} DisableRetryInit
+ * @property {'RETRY_INIT_DISABLE'} type
+ *
+ * @typedef {import('./ipfs-provider').Message | AppIdle | DisableRetryInit} Message
+ *
  * @typedef {Object} Model
  * @property {number} [startedAt]
  * @property {number} [failedAt]
+ * @property {number} [tryCount]
+ * @property {boolean} [needToRetry]
+ * @property {number} [intervalId]
+ * @property {boolean} currentlyTrying
  *
  * @typedef {Object} State
  * @property {Model} retryInit
+ *
  */
+
+const retryTime = 2500
+const maxRetries = 5
+
+/**
+ * @returns {Model}
+ */
+const initialState = () => ({ tryCount: 0, needToRetry: true, startedAt: undefined, failedAt: undefined, currentlyTrying: false })
+
+/**
+ * @returns {Model}
+ */
+const disabledState = () => {
+  return ({ ...initialState(), needToRetry: false })
+}
 
 // We ask for the stats every few seconds, so that gives a good indication
 // that ipfs things are working (or not), without additional polling of the api.
+
 const retryInit = {
   name: 'retryInit',
 
@@ -21,19 +50,30 @@ const retryInit = {
    * @param {Message} action
    * @returns {Model}
    */
-  reducer: (state = {}, action) => {
+  reducer: (state = initialState(), action) => {
     switch (action.type) {
+      case 'RETRY_INIT_DISABLE': {
+        return disabledState()
+      }
       case ACTIONS.IPFS_INIT: {
         const { task } = action
         switch (task.status) {
           case 'Init': {
-            return { ...state, startedAt: Date.now() }
+            const startedAt = Date.now()
+            return {
+              ...state,
+              currentlyTrying: true,
+              startedAt, // new init attempt, set startedAt
+              tryCount: (state.tryCount || 0) + 1 // increase tryCount
+            }
           }
           case 'Exit': {
             if (task.result.ok) {
-              return state
+              // things are okay, reset the state
+              return disabledState()
             } else {
-              return { ...state, failedAt: Date.now() }
+              const failedAt = Date.now()
+              return { ...state, failedAt, currentlyTrying: false }
             }
           }
           default: {
@@ -48,27 +88,40 @@ const retryInit = {
   },
 
   /**
-   * @param {State} state
+   * @returns {(context: import('redux-bundler').Context<Model, Message, unknown>) => void}
    */
-  selectInitStartedAt: state => state.retryInit.startedAt,
+  doDisableRetryInit: () => (context) => {
+    // we should emit IPFS_INIT_FAILED at this point
+    context.dispatch({
+      type: 'RETRY_INIT_DISABLE'
+    })
+  },
 
   /**
    * @param {State} state
    */
-  selectInitFailedAt: state => state.retryInit.failedAt,
+  selectRetryInitState: state => state.retryInit,
 
+  /**
+   * This is continuously called by the app
+   * @see https://reduxbundler.com/api-reference/bundle#bundle.reactx
+   */
   reactConnectionInitRetry: createSelector(
-    'selectAppTime',
-    'selectInitStartedAt',
-    'selectInitFailedAt',
+    'selectAppTime', // this is the current time of the app.. we need this to compare against startedAt
+    'selectIpfsReady',
+    'selectRetryInitState',
     /**
-     * @param {number} appTime
-     * @param {number|void} startedAt
-     * @param {number|void} failedAt
+     * @param {number|void} appTime
+     * @param {boolean} ipfsReady
+     * @param {Model} state
      */
-    (appTime, startedAt, failedAt) => {
-      if (!failedAt || failedAt < startedAt) return false
-      if (appTime - failedAt < 3000) return false
+    (appTime, ipfsReady, { failedAt, tryCount, needToRetry, currentlyTrying }) => {
+      if (currentlyTrying) return false // if we are currently trying, don't try again
+      if (!appTime) return false // This should never happen; see https://reduxbundler.com/api-reference/included-bundles#apptimebundle
+      if (!needToRetry) return false // we should not be retrying, so don't.
+      if (tryCount != null && tryCount > maxRetries) return { actionCreator: 'doDisableRetryInit' }
+      if (ipfsReady) return { actionCreator: 'doDisableRetryInit' } // when IPFS is ready, we don't need to retry
+      if (!failedAt || appTime - failedAt < retryTime) return false
       return { actionCreator: 'doTryInitIpfs' }
     }
   )
