0312257b30022c246853f3509625a319470b7abc
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-02-02 09:55:36 -0800

    fix(peers): add connection accepts valid multiaddrs

--- test/helpers/cid.js
+++ test/helpers/cid.js
@@ -1,10 +1,13 @@
-import multihashing from 'multihashing-async'
 import { promisify } from 'util'
 import Crypto from 'crypto'
-import { CID } from 'multiformats'
+import { CID } from 'multiformats/cid'
+import { sha256 } from 'multiformats/hashes/sha2'
+import * as dagPb from '@ipld/dag-pb'
 
 export async function fakeCid () {
-  const bytes = await promisify(Crypto.randomBytes)(Math.round(Math.random() * 1000))
-  const mh = await multihashing(bytes, 'sha2-256')
-  return new CID(0, 'dag-pb', Uint8Array.from(mh))
+  const randomBytesBuffer = await promisify(Crypto.randomBytes)(Math.round(Math.random() * 1000))
+  const bytes = new Uint8Array(randomBytesBuffer)
+  const mh = await sha256.digest(bytes)
+
+  return CID.create(0, dagPb.code, mh)
 }
