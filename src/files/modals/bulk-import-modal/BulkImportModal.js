e9b4d8585823e92c6929bc72f6e5fa4fb857c6f0
MattWong-ca <matthew.yt.wong@gmail.com>
2024-12-13 14:38:21 -0500

    Use functional component

--- src/files/modals/bulk-import-modal/BulkImportModal.js
+++ src/files/modals/bulk-import-modal/BulkImportModal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import Button from '../../../components/button/button.tsx'
 import { Modal, ModalActions, ModalBody } from '../../../components/modal/Modal.js'
 import { withTranslation } from 'react-i18next'
@@ -6,17 +6,12 @@ import * as isIPFS from 'is-ipfs'
 import Icon from '../../../icons/StrokeDocument.js'
 import { normalizeFiles } from '../../../lib/files.js'
 
-class BulkImportModal extends React.Component {
-  static defaultProps = {
-    className: ''
-  }
+const BulkImportModal = ({ t, tReady, onCancel, className = '', onBulkCidImport, ...props }) => {
+  const [selectedFile, setSelectedFile] = useState(null)
+  const [validationError, setValidationError] = useState(null)
+  const bulkCidInputRef = useRef(null)
 
-  state = {
-    selectedFile: null,
-    validationError: null
-  }
-
-  validateFileContents = async (file) => {
+  const validateFileContents = async (file) => {
     try {
       const text = await file.text()
       const lines = text.split('\n').filter(line => line.trim())
@@ -24,95 +19,84 @@ class BulkImportModal extends React.Component {
       for (const line of lines) {
         const [cid] = line.trim().split(' ')
         if (!isIPFS.cid(cid)) {
-          return { isValid: false, error: this.props.t('bulkImportModal.invalidCids') }
+          return { isValid: false, error: t('bulkImportModal.invalidCids') }
         }
       }
 
       return { isValid: true }
     } catch (err) {
-      return { isValid: false, error: this.props.t('bulkImportModal.failedToReadFile') }
+      return { isValid: false, error: t('bulkImportModal.failedToReadFile') }
     }
   }
 
-  onChange = async (event) => {
+  const onChange = async (event) => {
     const file = event.target.files[0]
     if (!file) return
 
-    const validation = await this.validateFileContents(file)
-    this.setState({
-      selectedFile: validation.isValid ? file : null,
-      validationError: validation.error
-    })
+    const validation = await validateFileContents(file)
+    setSelectedFile(validation.isValid ? file : null)
+    setValidationError(validation.error)
   }
 
-  selectFile = async () => {
-    return this.bulkCidInput.click()
+  const selectFile = () => {
+    bulkCidInputRef.current.click()
   }
 
-  onSubmit = async () => {
-    if (this.state.selectedFile) {
-      const normalizedFiles = normalizeFiles([this.state.selectedFile])
-      await this.props.onBulkCidImport(normalizedFiles)
+  const onSubmit = async () => {
+    if (selectedFile) {
+      const normalizedFiles = normalizeFiles([selectedFile])
+      await onBulkCidImport(normalizedFiles)
     }
   }
 
-  get isDisabled () {
-    return !this.state.selectedFile
-  }
+  const isDisabled = !selectedFile
+  const codeClass = 'w-100 mb1 pa1 tl bg-snow f7 charcoal-muted truncate'
 
-  render () {
-    const {
-      t, tReady, onCancel, className, onBulkCidImport, ...props
-    } = this.props
+  return (
+    <Modal {...props} className={className} onCancel={onCancel}>
+      <ModalBody title={t('bulkImportModal.title')} Icon={Icon}>
+        <div className='mb3 flex flex-column items-center'>
+          <p className='mt0 charcoal tl w-100'>{t('bulkImportModal.description')}</p>
+          <code className={codeClass}>bafkreibm6jg3ux5qumhcn2b3flc3tyu6dmlb4xa7u5bf44yegnrjhc4yeq<br />QmawceGscqN4o8Y8Fv26UUmB454kn2bnkXV5tEQYc4jBd6 barrel.png<br />QmbvrHYWXAU1BuxMPNRtfeF4DS2oPmo5hat7ocqAkNPr74 pi equals.png</code>
+        </div>
 
-    const codeClass = 'w-100 mb1 pa1 tl bg-snow f7 charcoal-muted truncate'
+        <input
+          id='bulk-import'
+          type='file'
+          className='dn'
+          multiple
+          accept='.txt'
+          ref={bulkCidInputRef}
+          onChange={onChange}
+        />
+        <Button
+          onClick={selectFile}
+          className='ma2 tc'
+          bg='bg-teal'
+          type='button'
+        >
+          {t('bulkImportModal.select')}
+        </Button>
 
-    return (
-      <Modal {...props} className={className} onCancel={onCancel}>
-        <ModalBody title={t('bulkImportModal.title')} Icon={Icon}>
-          <div className='mb3 flex flex-column items-center'>
-            <p className='mt0 charcoal tl w-100'>{t('bulkImportModal.description')}</p>
-            <code className={codeClass}>bafkreibm6jg3ux5qumhcn2b3flc3tyu6dmlb4xa7u5bf44yegnrjhc4yeq<br/>QmawceGscqN4o8Y8Fv26UUmB454kn2bnkXV5tEQYc4jBd6 barrel.png<br/>QmbvrHYWXAU1BuxMPNRtfeF4DS2oPmo5hat7ocqAkNPr74 pi equals.png</code>
-          </div>
+        {selectedFile && (
+          <p className='mt2 charcoal'>
+            {`${t('bulkImportModal.selectedFile')}: ${selectedFile.name}`}
+          </p>
+        )}
 
-          <input
-            id='bulk-import'
-            type='file'
-            className='dn'
-            multiple
-            accept='.txt'
-            ref={el => { this.bulkCidInput = el }}
-            onChange={this.onChange}
-          />
-          <Button
-            onClick={this.selectFile}
-            className='ma2 tc'
-            bg='bg-teal'
-            type='button'
-          >
-            {t('bulkImportModal.select')}
-          </Button>
+        {validationError && (
+          <p className='mt2 red'>
+            {validationError}
+          </p>
+        )}
+      </ModalBody>
 
-          {this.state.selectedFile && (
-            <p className='mt2 charcoal'>
-              {`${t('bulkImportModal.selectedFile')}: ${this.state.selectedFile.name}`}
-            </p>
-          )}
-
-          {this.state.validationError && (
-            <p className='mt2 red'>
-              {this.state.validationError}
-            </p>
-          )}
-        </ModalBody>
-
-        <ModalActions>
-          <Button className='ma2 tc' bg='bg-gray' onClick={onCancel}>{t('actions.cancel')}</Button>
-          <Button className='ma2 tc' bg='bg-teal' disabled={this.isDisabled} onClick={this.onSubmit}>{t('app:actions.import')}</Button>
-        </ModalActions>
-      </Modal>
-    )
-  }
+      <ModalActions>
+        <Button className='ma2 tc' bg='bg-gray' onClick={onCancel}>{t('actions.cancel')}</Button>
+        <Button className='ma2 tc' bg='bg-teal' disabled={isDisabled} onClick={onSubmit}>{t('app:actions.import')}</Button>
+      </ModalActions>
+    </Modal>
+  )
 }
 
 export default withTranslation('files')(BulkImportModal)
