180dd54873b2a859514c39f1eb12754073fabc23
Caleb! <48127194+kaf-lamed-beyt@users.noreply.github.com>
2025-02-11 21:12:24 +0100

    Update src/hooks/theme.ts
    Co-authored-by: Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>

--- src/hooks/theme.ts
+++ src/hooks/theme.ts
@@ -3,7 +3,7 @@ import { ThemeContext, ThemeContextValues } from '../context/theme-provider'
 
 export const useTheme = () => {
   const context = React.useContext(ThemeContext)
-  if(context === null) {
+  if (context == null) {
     throw new Error('Theme context is missing You probably forgot to wrap the component depending on theme in <ThemeProvider />')
   }
   return context as ThemeContextValues
