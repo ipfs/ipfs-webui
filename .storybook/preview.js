a85bdc2863da8c69114e0825589cb2f000b986ab
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-02-27 10:31:50 -0600

    Revert "feat: add dark mode (#2322)" (#2339)
    This reverts commit d751fc63903bc72086b177f82bc4933136e89d0f.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,7 +9,6 @@ import getStore from '../src/bundles/index.js'
 import i18n from '../src/i18n.js'
 import DndBackend from '../src/lib/dnd-backend.js'
 import { HeliaProvider, ExploreProvider } from 'ipld-explorer-components/providers'
-import { ThemeProvider } from '../src/context/theme-provider.tsx'
 
 /**
  * @type {import('@storybook/addons').BaseAnnotations}
@@ -17,19 +16,17 @@ import { ThemeProvider } from '../src/context/theme-provider.tsx'
 const baseAnnotations = {
   decorators: [
     (Story) => (
-      <ThemeProvider>
-        <Provider store={getStore(undefined)}>
-          <I18nextProvider i18n={i18n} >
-            <DndProvider backend={DndBackend}>
-              <HeliaProvider>
-                <ExploreProvider>
-                  <Story />
-                </ExploreProvider>
-              </HeliaProvider>
-            </DndProvider>
-          </I18nextProvider>
-        </Provider>
-      </ThemeProvider>
+      <Provider store={getStore(undefined)}>
+        <I18nextProvider i18n={i18n} >
+          <DndProvider backend={DndBackend}>
+            <HeliaProvider>
+              <ExploreProvider>
+                <Story />
+              </ExploreProvider>
+            </HeliaProvider>
+          </DndProvider>
+        </I18nextProvider>
+      </Provider>
     )
   ],
   /**
--- src/components/box/Box.js
+++ src/components/box/Box.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import { useTheme } from '../../hooks/theme'
 import ErrorBoundary from '../error/ErrorBoundary.js'
 
 export const Box = ({
   className = 'pa4',
   style,
-  themed,
   children,
   ...props
 }) => {
-  const { isDarkTheme } = useTheme()
   return (
-    <section className={className} style={{ background: isDarkTheme ? 'var(--element-bg)' : 'var(--element-bg-light)', ...style }}>
+    <section className={className} style={{ background: '#fbfbfb', ...style }}>
       <ErrorBoundary>
         {children}
       </ErrorBoundary>
