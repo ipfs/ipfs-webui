a0049bc95fe7176d17189ae36670cdea26f4153b
Shobit garg <122355051+shobit000@users.noreply.github.com>
2025-03-16 22:26:23 +0530

    Update ipns.js

--- src/bundles/ipns.js
+++ src/bundles/ipns.js
@@ -47,14 +47,12 @@ const ipnsBundle = {
   doRemoveIpnsKey: (name) => async ({ getIpfs, store }) => {
     const ipfs = getIpfs()
     await ipfs.key.rm(name)
-
     store.doFetchIpnsKeys()
   },
 
   doRenameIpnsKey: (oldName, newName) => async ({ getIpfs, store }) => {
     const ipfs = getIpfs()
     await ipfs.key.rename(oldName, newName)
-
     store.doFetchIpnsKeys()
   },
 
@@ -63,9 +61,31 @@ const ipnsBundle = {
     await ipfs.name.publish(cid, { key })
   },
 
+  doImportIpnsKey: (file) => async ({ getIpfs, store }) => {
+    const ipfs = getIpfs()
+    const reader = new FileReader()
+    reader.onload = async (event) => {
+      const key = event.target.result
+      await ipfs.key.import(file.name, key)
+      store.doFetchIpnsKeys()
+    }
+    reader.readAsText(file)
+  },
+
+  doExportIpnsKey: (name) => async ({ getIpfs }) => {
+    const ipfs = getIpfs()
+    const key = await ipfs.key.export(name)
+    const blob = new Blob([key], { type: 'text/plain' })
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = `${name}.key`
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+  },
+
   doUpdateExpectedPublishTime: (time) => async ({ store, dispatch }) => {
-    // moderate expectation: publishing should take no longer than average
-    // between old expectation and the length of the last publish + some buffer
     const oldExpectedTime = store.selectExpectedPublishTime()
     const avg = Math.floor((time * 1.5 + oldExpectedTime) / 2)
     await writeSetting('expectedPublishTime', avg)
