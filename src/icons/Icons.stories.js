ce3c8c91b417db018d6588b9379eb8208a80e599
Suchitra Swain <suchitraswain.2012@gmail.com>
2025-08-05 11:23:46 +0200

    Anonymous object

--- src/icons/Icons.stories.js
+++ src/icons/Icons.stories.js
@@ -1,76 +1,91 @@
-import React from 'react'
+import React from "react";
 
-import * as iconImports from './index.js'
+import * as iconImports from "./index.js";
 const icons = Object.keys(iconImports).map((m) => ({
   name: m,
-  Icon: iconImports[m]
-}))
+  Icon: iconImports[m],
+}));
 
 const filterByTextQuery = (icon, searchQuery) => {
-  return icon.name.toLowerCase().includes(searchQuery.toLowerCase())
-}
+  return icon.name.toLowerCase().includes(searchQuery.toLowerCase());
+};
 
 /**
  * @type {import('@storybook/react').StoryObj}
  */
 const List = ({ iconFilter, size, fill, stroke }) => {
-  const iconFilterNotSet = iconFilter == null || iconFilter.length === 0
+  const iconFilterNotSet = iconFilter == null || iconFilter.length === 0;
 
   return (
     <>
-      <span className="gray">{iconFilterNotSet ? '* Use Storybook controls for prop "iconFilter" to filter icons' : `* Currently filtering icons for "${iconFilter}"`}</span>
+      <span className="gray">
+        {iconFilterNotSet
+          ? '* Use Storybook controls for prop "iconFilter" to filter icons'
+          : `* Currently filtering icons for "${iconFilter}"`}
+      </span>
       <div className="flex w-100 flex-wrap">
-        {icons.filter((i) => filterByTextQuery(i, iconFilter)).map(({ Icon, name }) => (
-          <div key={name} className="flex items-center flex-column ma3">
-            <Icon fill={fill} stroke={stroke} width={size} height={size} className="transition-all" />
-            <span>{name}</span>
-          </div>
-        ))}
+        {icons
+          .filter((i) => filterByTextQuery(i, iconFilter))
+          .map(({ Icon, name }) => (
+            <div key={name} className="flex items-center flex-column ma3">
+              <Icon
+                fill={fill}
+                stroke={stroke}
+                width={size}
+                height={size}
+                className="transition-all"
+              />
+              <span>{name}</span>
+            </div>
+          ))}
       </div>
     </>
-  )
-}
+  );
+};
 
 /**
  * @type {import('@storybook/react').Meta}
  */
-export default {
-  title: 'Icons',
+const meta = {
+  title: "Icons",
   component: List,
   // decorators: [withKnobs],
   argTypes: {
     iconFilter: {
-      control: { type: 'text' }
+      control: { type: "text" },
     },
     size: {
       control: {
-        type: 'range',
+        type: "range",
         min: 1,
         max: 200,
-        step: 1
-      }
+        step: 1,
+      },
     },
     fill: {
       control: {
-        type: 'color'
-      }
+        type: "color",
+      },
     },
     stroke: {
       control: {
-        type: 'color'
-      }
-    }
-  }
-}
+        type: "color",
+      },
+    },
+  },
+};
+
+export default meta;
+
 /**
  * @type {import('@storybook/react').StoryObj}
  */
 export const Default = {
-  name: 'Icons',
+  name: "Icons",
   args: {
-    iconFilter: '',
+    iconFilter: "",
     size: 32,
     fill: undefined,
-    stroke: undefined
-  }
-}
+    stroke: undefined,
+  },
+};
