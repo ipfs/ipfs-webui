efb53eb051b7ddc86340f9f567952e3d4a140eeb
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-08-12 14:39:52 -0400

    chore: no newlines between imports

--- src/files/file-not-found/index.tsx
+++ src/files/file-not-found/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-
 import Button from '../../components/button/button.js'
-
 import GlyphAttention from '../../icons/GlyphAttention.js'
 import { useTranslation } from 'react-i18next'
 
