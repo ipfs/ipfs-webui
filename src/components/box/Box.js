b32f096582628da40079bc3bbaf6838bc59070ac
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-08-25 15:28:33 -0400

    fix: typecheck src/welcome

--- src/components/box/Box.js
+++ src/components/box/Box.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import ErrorBoundary from '../error/error-boundary.js'
 
+/**
+ * @param {Object} props
+ * @param {string} [props.className]
+ * @param {React.CSSProperties} [props.style]
+ * @param {React.ReactNode} props.children
+ */
 export const Box = ({
   className = 'pa4',
   style,
-  children,
-  ...props
+  children
 }) => {
   return (
     <section className={className} style={{ background: '#fbfbfb', ...style }}>
--- src/components/tour/withTour.js
+++ src/components/tour/withTour.js
@@ -2,8 +2,15 @@ import React from 'react'
 import { connect } from 'redux-bundler-react'
 import { STATUS } from 'react-joyride'
 
+/**
+ * @param {React.ComponentType<any>} WrappedComponent
+ * @returns {React.ComponentType<any>}
+ */
 const withTour = WrappedComponent => {
   class WithTour extends React.Component {
+    /**
+     * @param {any} data
+     */
     handleJoyrideCallback = (data) => {
       const { doDisableTours } = this.props
       const { action, status } = data
